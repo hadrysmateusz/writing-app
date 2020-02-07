dc3f981041689d0877807a0de9c23dbca2dbae35
Mateusz Hadrys <hadrys.mateusz86@gmail.com>
2020-02-07 22:05:15 +0100

    change hosting url

--- packages/constants/src/index.js
+++ packages/constants/src/index.js
@@ -3,5 +3,5 @@ import * as routes from "./routes"
 export const ROUTES = routes
 export const MEDIUM_API_BASE_URL = "https://api.medium.com/v1"
 export const MEDIUM_API_AUTH_URL = "https://medium.com/m/oauth/authorize"
-export const HOSTING_URL = "https://write-and-publish-tool.web.app"
+export const HOSTING_URL = "https://write-and-publish-tool.firebaseapp.com"
 export const CLOUD_FUNCTIONS_BASE_URL = "https://us-central1-write-and-publish-tool.cloudfunctions.net"
