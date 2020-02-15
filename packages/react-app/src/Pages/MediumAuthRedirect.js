f8db2af5b3fc0eadc8424d04e765237fa301b5e7
Mateusz Hadryś <ja@pop-os.localdomain>
2020-02-15 13:49:29 +0100

    fix some lingering issues with medium auth error handling

--- packages/react-app/src/Pages/MediumAuthRedirect.js
+++ packages/react-app/src/Pages/MediumAuthRedirect.js
@@ -11,43 +11,45 @@ export default () => {
 	const [error, setError] = useState(null)
 
 	useEffect(() => {
+		// intentionally outside the inner trycatch block
+		if (!window.opener) {
+			// consider simply logging the error and redirecting to home page (maybe with some params that will display a flash message)
+			setError("This page shouldn't be accessed directly")
+			console.error("Medium Auth Redirect Page has no window.opener")
+			return
+		}
+
 		try {
-			// intentionally outside the inner trycatch block
-			if (!window.opener) {
-				throw Error("Medium Auth Redirect Page has no window.opener")
+			const params = new URLSearchParams(window.location.search)
+
+			if (params.has("error")) {
+				throw Error("Medium Error:" + params.error)
+			}
+			if (!params.has("state") || !params.has("code")) {
+				throw Error("Response is missing required search parameters")
 			}
 
-			try {
-				const params = new URLSearchParams(window.location.search)
+			const state = params.get("state")
+			const code = params.get("code")
 
-				if (params.has("error")) {
-					throw Error("Medium Error:" + params.error)
-				}
-				if (!params.has("state") || !params.has("code")) {
-					throw Error("Response is missing required search parameters")
-				}
-
-				const state = params.get("state")
-				const code = params.get("code")
-
-				// TODO: verify that using window.opener is safe, it might be necessary to hardcode a url or use a different solution
-				// send the parameters to the opening window
-				window.opener.postMessage({
-					type: MESSAGE_TYPES.MEDIUM_AUTH_CALLBACK,
-					state,
-					code
-				})
-			} catch (error) {
-				console.log(error)
-				// send the error message to the opening window
-				window.opener.postMessage({ type: MESSAGE_TYPES.MEDIUM_AUTH_CALLBACK, error })
-			}
-
+			// TODO: verify that using window.opener is safe, it might be necessary to hardcode a url or use a different solution
+			// send the parameters to the opening window
+			window.opener.postMessage({
+				type: MESSAGE_TYPES.MEDIUM_AUTH_CALLBACK,
+				state,
+				code
+			})
+		} catch (err) {
+			console.log(err)
+			setError(err.message)
+			// send the error message to the opening window
+			window.opener.postMessage({
+				type: MESSAGE_TYPES.MEDIUM_AUTH_CALLBACK,
+				error: err.message
+			})
+		} finally {
 			// close the popup
 			window.close()
-		} catch (error) {
-			console.log(error)
-			setError(error.message || "Unknown error")
 		}
 	}, [])
 
