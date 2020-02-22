1ff8920f082fcf47d43e437490bc854a0b42c3d0
Mateusz Hadryś <ja@pop-os.localdomain>
2020-02-22 13:26:07 +0100

    fixed config persistance

--- packages/dev-tools/src/config.js
+++ packages/dev-tools/src/config.js
@@ -1,21 +1,26 @@
-// TODO: when a feature toggle is removed, the corresponding value isn't removed from localStorage
-
 let config
+const key = "config"
 
 const loadConfig = (_config, callback) => {
-	console.log("loading config: ", _config)
-
-	config = _config
+	console.groupCollapsed("LOADING CONFIG")
+	const localConfig = getConfigFromLocalStorage()
+	console.log("default config" + _config)
+	console.log("local config" + localConfig)
+	// TODO: when a feature toggle is removed, the corresponding value isn't removed from localStorage
+	config = { ..._config, ...localConfig } // config in localStorage should override the defaults
+	console.log("merged config" + config)
+	console.groupEnd()
+	// call the callback function ()
 	callback()
 }
 
-const key = "config"
-
-// update config with what's in localStorage
-try {
-	Object.assign(config, JSON.parse(window.localStorage.getItem(key)))
-} catch (error) {
-	window.localStorage.removeItem(key)
+const getConfigFromLocalStorage = () => {
+	try {
+		return JSON.parse(window.localStorage.getItem(key))
+	} catch (error) {
+		window.localStorage.removeItem(key)
+		return {}
+	}
 }
 
 const persist = () => window.localStorage.setItem(key, JSON.stringify(config))
