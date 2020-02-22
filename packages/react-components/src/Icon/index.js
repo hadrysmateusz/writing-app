8d41624489ee842e419817b90764571ec84d95c7
Mateusz Hadryś <ja@pop-os.localdomain>
2020-02-22 22:31:02 +0100

    make Icon use slate formatting names for icon names

--- packages/react-components/src/Icon/index.js
+++ packages/react-components/src/Icon/index.js
@@ -15,24 +15,29 @@ import {
 import { ReactComponent as Heading1 } from "../Assets/Heading1.svg"
 import { ReactComponent as Heading2 } from "../Assets/Heading2.svg"
 
+import { BLOCKS, MARKS, INLINES } from "@writing-tool/constants/src/Slate"
+
 const Icons = {
-	bold: FaBold,
-	italic: FaItalic,
-	strikethrough: FaStrikethrough,
-	code: FaCode,
-	link: FaLink,
-	quote: FaQuoteRight,
-	image: FaRegFileImage,
-	listNumbered: FaListOl,
-	listBulleted: FaListUl,
-	heading1: Heading1,
-	heading2: Heading2
+	[MARKS.BOLD]: FaBold,
+	[MARKS.ITALIC]: FaItalic,
+	[MARKS.STRIKE]: FaStrikethrough,
+	[MARKS.CODE]: FaCode,
+	[INLINES.LINK]: FaLink,
+	[BLOCKS.CODE]: FaCode,
+	[BLOCKS.BLOCKQUOTE]: FaQuoteRight,
+	[BLOCKS.IMAGE]: FaRegFileImage,
+	[BLOCKS.LIST_NUMBERED]: FaListOl,
+	[BLOCKS.LIST_BULLETED]: FaListUl,
+	[BLOCKS.HEADING_1]: Heading1,
+	[BLOCKS.HEADING_2]: Heading2
 }
 
 function Icon({ icon }) {
 	const iconComponent = Icons[icon]
 	if (!iconComponent) console.error("invalid icon:", icon)
-	return iconComponent ? <IconContainer>{React.createElement(iconComponent, null, null)}</IconContainer> : null
+	return iconComponent ? (
+		<IconContainer>{React.createElement(iconComponent, null, null)}</IconContainer>
+	) : null
 }
 
 const IconContainer = styled.div``
