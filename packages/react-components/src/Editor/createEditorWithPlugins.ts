6542d0d45ea0729bf8707c063c3e8b5b28413472
Mateusz Hadryś <ja@pop-os.localdomain>
2020-03-01 01:03:59 +0100

    more typescript conversion (still broken)

--- packages/react-components/src/Editor/createEditorWithPlugins.ts
+++ packages/react-components/src/Editor/createEditorWithPlugins.ts
@@ -1,10 +1,13 @@
 import { useMemo } from "react"
 import { createEditor } from "slate"
+import { ReactEditor } from "slate-react"
 
 import { compose } from "lodash/fp"
 
-function useCreateEditor(plugins) {
-	return useMemo(compose(...plugins, createEditor)(), [])
+function useCreateEditor(plugins): ReactEditor {
+	return useMemo(() => {
+		return compose(...plugins, createEditor)()
+	}, [plugins])
 }
 
 export default useCreateEditor
