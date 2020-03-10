fe3b02c80f4bb7c8ad0eb3708b8de3ea0dea20e3
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-03-10 01:21:56 +0100

    reworked getSelectedNodes
    a lot of optimalization still needs to be done

--- packages/slate-helpers/src/getSelectedNodes.ts
+++ packages/slate-helpers/src/getSelectedNodes.ts
@@ -1,6 +1,16 @@
 import { Editor, Element, Path, Node, NodeEntry } from "slate"
 
-export const getSelectedNodes = (editor): NodeEntry[] => {
+export const getLastPathIndex = (path) => path[path.length - 1]
+
+export const getSelectedNodes = (
+	editor
+): {
+	parentPath: Path
+	relativePaths: number[]
+	fullPaths: Path[]
+	nodes: Node[]
+	parentNode: Node
+} => {
 	const [...nodes] = Editor.nodes(editor, {
 		mode: "lowest",
 		match: (n) => !Editor.isEditor(n) && Element.isElement(n) && !editor.isInline(n)
@@ -8,14 +18,23 @@ export const getSelectedNodes = (editor): NodeEntry[] => {
 
 	if (!editor.selection) {
 		console.warn("attempted to move nodes without a selection")
-		return []
+		return {
+			parentPath: null,
+			relativePaths: null,
+			fullPaths: [],
+			nodes: [],
+			parentNode: null
+		}
 	}
 
-	const commonPaths = getCommonPaths(editor, nodes, [], 0)
-	return commonPaths.map((path) => {
-		const node = Node.get(editor, path)
-		return [node, path]
-	})
+	const { parentPath, relativePaths, fullPaths } = getCommonPaths(editor, nodes, [], 0)
+	return {
+		parentPath,
+		relativePaths,
+		fullPaths,
+		parentNode: Node.get(editor, parentPath),
+		nodes: fullPaths.map((path) => Node.get(editor, path))
+	}
 }
 
 const getCommonPaths = (
@@ -23,26 +42,34 @@ const getCommonPaths = (
 	nodeEntries: NodeEntry<Node>[],
 	commonPath: Path,
 	index: number
-): Path[] => {
+): { parentPath: Path; relativePaths: number[]; fullPaths: Path[] } => {
 	// if there are no nodes there will be no commonPath paths
 	if (nodeEntries.length === 0) {
 		console.log("no node entries. returning empty...")
-		return []
+		return { parentPath: null, relativePaths: null, fullPaths: [] }
 	}
 
-	// if there is only one node, there will only be one path
+	// if there is only one NodeEntry, there will only be one path
 	if (nodeEntries.length === 1) {
-		console.log("there is only one node...")
-		// nodeEntries is an array of tuples ([Node,Path]) and we only need the path
-		const [, path] = nodeEntries[0]
-		const parent = Node.parent(editor, path)
-		const numChildren = parent.children.length
-		if (numChildren === 1) {
-			console.log("it's the parent's only, child. returning parent...")
-			return [Path.parent(path)]
+		const [, path] = nodeEntries[0] // nodeEntries is an array of tuples ([Node,Path]) and we only need the path
+		const parentPath = Path.parent(path)
+		const parentNode = Node.get(editor, parentPath)
+		const numChildren = parentNode.children.length
+
+		// if the node is an only child, it's parent should be returned instead (if it's not the editor)
+		if (numChildren === 1 && !Editor.isEditor(parentNode)) {
+			console.log("rounding the selection to parent...")
+			return {
+				parentPath: Path.parent(parentPath),
+				relativePaths: [getLastPathIndex(parentPath)],
+				fullPaths: [parentPath]
+			}
 		} else {
-			console.log("parent has multiple children. returning child...")
-			return [path]
+			return {
+				parentPath: parentPath,
+				relativePaths: [getLastPathIndex(path)],
+				fullPaths: [path]
+			}
 		}
 	}
 
@@ -62,25 +89,43 @@ const getCommonPaths = (
 	if (numSelected > 1) {
 		// if there were many unique values it means that the common part is over
 
+		// if the common node is the editor, return the children
+		const commonNode = Node.get(editor, commonPath)
+		if (Editor.isEditor(commonNode)) {
+			const fullPaths = values.map((value) => [...commonPath, value])
+			return { parentPath: commonPath, relativePaths: values, fullPaths: fullPaths }
+		}
+
 		const [...children] = Node.children(editor, commonPath)
 		const numChildren = children.length
 		const commonMsg = `${numSelected}/${numChildren} of the common parent's children are selected.`
 		const allChildrenSelected = numChildren === numSelected
+		const parentPath = Path.parent(commonPath)
+		const parentNode = Node.get(editor, parentPath)
 
-		if (allChildrenSelected) {
+		if (allChildrenSelected && !Editor.isEditor(parentNode)) {
 			// if all of the common parent's children are selected we should treat it as a selection on the parent itself
 			console.log(`${commonMsg} returning parent...`)
-			return [commonPath]
+			return {
+				parentPath: Path.parent(commonPath),
+				relativePaths: [0],
+				fullPaths: [commonPath]
+			}
 		} else {
 			// if there were many unique values it means that the common part is over and we can return the paths
 			// we construct the paths by concatenating the common part with all unique values
 			console.log(`${commonMsg} returning children...`)
-			return values.map((value) => [...commonPath, value])
+			const fullPaths = values.map((value) => [...commonPath, value])
+			return { parentPath: commonPath, relativePaths: values, fullPaths: fullPaths }
 		}
 	} else if (numSelected === 0) {
 		// if there was no values at all, it probably means that the nodeEntries were duplicated and we should treat the common part as the only common path
 		console.warn("unusual scenario, returning common part")
-		return [commonPath]
+		return {
+			parentPath: Path.parent(commonPath),
+			relativePaths: [0],
+			fullPaths: [commonPath]
+		}
 	} else {
 		// if there was only one unique value it means that we are still in the common part and we need to move further
 		return getCommonPaths(editor, nodeEntries, [...commonPath, values[0]], index + 1)
