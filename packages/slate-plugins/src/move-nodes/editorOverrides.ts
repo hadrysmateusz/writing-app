b96089b491799f74e5138421baa00a950b33aaea
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-03-13 21:11:07 +0100

    re-add the multi-line selection highlight

--- packages/slate-plugins/src/move-nodes/editorOverrides.ts
+++ packages/slate-plugins/src/move-nodes/editorOverrides.ts
@@ -1,6 +1,6 @@
-import { Operation, Editor, Transforms } from "slate"
+import { Operation } from "slate"
 import { ReactEditor } from "slate-react"
-import { getSelectedNodes } from "@writing-tool/slate-helpers"
+import { getSelectedNodes, isSelectionMultiLine } from "@writing-tool/slate-helpers"
 
 export const withMoveNodes = (editor) => {
 	const { apply } = editor
@@ -8,25 +8,21 @@ export const withMoveNodes = (editor) => {
 	editor.apply = (op) => {
 		apply(op)
 
-		// if (Operation.isSelectionOperation(op)) {
-		// 	const [...elements] = document.getElementsByClassName("slate-node-selected")
-		// 	elements.forEach((element) => {
-		// 		element.classList.remove("slate-node-selected")
-		// 	})
+		if (Operation.isSelectionOperation(op)) {
+			const [...elements] = document.getElementsByClassName("slate-node-selected")
+			elements.forEach((element) => {
+				element.classList.remove("slate-node-selected")
+			})
 
-		// 	const entries = getSelectedNodes(editor)
+			if (isSelectionMultiLine(editor)) {
+				const { nodes } = getSelectedNodes(editor)
 
-		// 	// const firstPath = entries[0][1]
-		// 	// const lastPath = entries[entries.length - 1][1]
-		// 	// const start = Editor.start(editor, firstPath)
-		// 	// const end = Editor.end(editor, lastPath)
-		// 	// Transforms.setSelection(editor, { anchor: start, focus: end })
-
-		// 	entries.forEach(([node, path]) => {
-		// 		const DOMNode = ReactEditor.toDOMNode(editor, node)
-		// 		DOMNode.classList.add("slate-node-selected")
-		// 	})
-		// }
+				nodes.forEach((node) => {
+					const DOMNode = ReactEditor.toDOMNode(editor, node)
+					DOMNode.classList.add("slate-node-selected")
+				})
+			}
+		}
 	}
 
 	return editor
