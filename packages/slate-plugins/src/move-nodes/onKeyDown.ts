b0ef14fb8001bebff3b6d16ccac20c35851aaf11
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-03-13 19:32:49 +0100

    clean up the temporary solution

--- packages/slate-plugins/src/move-nodes/onKeyDown.ts
+++ packages/slate-plugins/src/move-nodes/onKeyDown.ts
@@ -1,21 +1,23 @@
-import { Transforms, Path, Node, Editor, NodeEntry, Point } from "slate"
+import { Transforms, Path, Node, Point } from "slate"
+
 import { ReactEditor } from "slate-react"
 import {
 	getSelectedNodes,
-	sortPaths,
-	sortNodeEntries,
 	getLastPathIndex,
 	isFirstChild,
 	isLastChild
 } from "@writing-tool/slate-helpers"
 
 const moveNodes = (editor, from: Path[], to: Path) => {
-	const selection = editor.selection
-	console.log("old selection", selection)
+	// TODO: This is a temporary solution - there are still many issues with it (see below). I'm going to wait for now, maybe the issue will be fixed at some point upstream, if not I can attempt to fix it myself, or (temporarily) restrict moving nodes to a collapse selection (single line)
 
+	const oldSelection = editor.selection
 	Transforms.deselect(editor)
+
+	// TODO: History is messed up after moving (when using the custom deselect/select logic)
+	// TODO: Moving multiple nodes when one of them is a list is broken (without the custom deselect/select logic)
 	Transforms.moveNodes(editor, {
-		at: selection,
+		at: oldSelection,
 		to,
 		match: (node) => {
 			const path = ReactEditor.findPath(editor, node)
@@ -30,46 +32,14 @@ const moveNodes = (editor, from: Path[], to: Path) => {
 		mode: "all"
 	})
 
-	const isSameParent = Node.parent(editor, to) === Node.parent(editor, from[0])
+	const oldPaths = from
+	const isSameParent = Node.parent(editor, to) === Node.parent(editor, oldPaths[0])
 	if (isSameParent) {
-		const numMovedElements = from.length
-		const lastIndex = getLastPathIndex(to)
-		const newPathBase = to.slice(0, -1)
-
-		const newPaths = from.map((oldPath, i) => {
-			if (Path.isBefore(to, from[0])) {
-				// moving up
-				return [...newPathBase].concat(lastIndex + i)
-			} else {
-				// moving down
-				return [...newPathBase].concat(lastIndex + 1 - numMovedElements + i)
-			}
-		})
-
-		console.log("new paths?:", newPaths)
-
-		const newAnchorBase = getNewPathBase(from, newPaths, selection.anchor)
-		const newFocusBase = getNewPathBase(from, newPaths, selection.focus)
-
-		const commonLength = from[0].length
-
-		console.log("anchor", selection.anchor.path.slice(0, commonLength), newAnchorBase)
-		console.log("focus", selection.focus.path.slice(0, commonLength), newFocusBase)
-
-		const newSelection = {
-			anchor: {
-				path: newAnchorBase.concat(selection.anchor.path.slice(commonLength)),
-				offset: selection.anchor.offset
-			},
-			focus: {
-				path: newFocusBase.concat(selection.focus.path.slice(commonLength)),
-				offset: selection.focus.offset
-			}
-		}
-
+		const newPaths = getPathsAfterMoving(oldPaths, to)
+		const newSelection = mapSelection(oldPaths, newPaths, oldSelection)
 		Transforms.select(editor, newSelection)
-
-		console.log(selection, newSelection)
+	} else {
+		// TODO: handle moving from nodes to a different parent (will need to handle unwrapping list-items - this might be handled in normalizing AFTER moving though)
 	}
 }
 
@@ -79,19 +49,7 @@ export const onKeyDownMoveNodes = () => (e: KeyboardEvent, editor: ReactEditor) 
 
 		if (!editor.selection) return
 
-		const { fullPaths, nodes, parentPath, relativePaths } = getSelectedNodes(
-			editor,
-			"asc"
-		)
-
-		console.log(
-			"parentPath",
-			parentPath,
-			"fullPaths",
-			fullPaths,
-			"relativePaths",
-			relativePaths
-		)
+		const { fullPaths } = getSelectedNodes(editor, "asc")
 
 		const firstPath = fullPaths[0]
 		const lastPath = fullPaths[fullPaths.length - 1]
@@ -103,17 +61,30 @@ export const onKeyDownMoveNodes = () => (e: KeyboardEvent, editor: ReactEditor) 
 			case "ArrowUp":
 				if (isFirstChild(firstPath)) break
 				newPath = Path.previous(firstPath)
-				console.log("moving nodes:", nodes, "to path: ", newPath)
 				moveNodes(editor, fullPaths, newPath)
 				break
 			case "ArrowDown":
 				if (isLastChild(editor, lastPath)) break
 				newPath = Path.next(lastPath)
-				console.log("moving nodes:", nodes, "to path: ", newPath)
 				moveNodes(editor, fullPaths, newPath)
 				break
 		}
 	}
+}
+
+const getPathsAfterMoving = (oldPaths: Path[], moveTarget: Path) => {
+	const numMovedElements = oldPaths.length
+	const lastIndex = getLastPathIndex(moveTarget)
+	const newPathBase = moveTarget.slice(0, -1)
+
+	return oldPaths.map((_, i) => {
+		// moving up and down needs different logic to account for the displacement of the moved nodes
+		if (Path.isBefore(moveTarget, oldPaths[0])) {
+			return [...newPathBase].concat(lastIndex + i) // moving up
+		} else {
+			return [...newPathBase].concat(lastIndex + 1 - numMovedElements + i) // moving down
+		}
+	})
 }
 
 const getNewPathBase = (oldPaths: Path[], newPaths: Path[], oldSelectionPoint: Point) => {
@@ -125,4 +96,22 @@ const getNewPathBase = (oldPaths: Path[], newPaths: Path[], oldSelectionPoint: P
 	})
 
 	return newPaths[index]
+}
+
+const mapSelection = (oldPaths, newPaths, selection) => {
+	const newAnchorBase = getNewPathBase(oldPaths, newPaths, selection.anchor)
+	const newFocusBase = getNewPathBase(oldPaths, newPaths, selection.focus)
+
+	const commonLength = oldPaths[0].length
+
+	return {
+		anchor: {
+			path: newAnchorBase.concat(selection.anchor.path.slice(commonLength)),
+			offset: selection.anchor.offset
+		},
+		focus: {
+			path: newFocusBase.concat(selection.focus.path.slice(commonLength)),
+			offset: selection.focus.offset
+		}
+	}
 }
