eb17e6c4e41356a598e3c01b0e72d2b6333f40e2
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-03-13 21:12:57 +0100

    remove the unnecessary vendor prefixes

--- packages/react-components/src/Editor/BlockWrapper.tsx
+++ packages/react-components/src/Editor/BlockWrapper.tsx
@@ -17,18 +17,8 @@ const Toolbar = styled.div`
 	font-weight: bold;
 	font-size: 20px;
 	cursor: pointer;
-	-webkit-touch-callout: none;
-	-webkit-user-select: none;
-	-khtml-user-select: none;
-	-moz-user-select: none;
-	-ms-user-select: none;
 	user-select: none;
 	* {
-		-webkit-touch-callout: none;
-		-webkit-user-select: none;
-		-khtml-user-select: none;
-		-moz-user-select: none;
-		-ms-user-select: none;
 		user-select: none;
 	}
 `
