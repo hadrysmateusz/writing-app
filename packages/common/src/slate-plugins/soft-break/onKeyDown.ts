3b0d74257dce9cbf6e17e1c3664be9da5d7a40d6
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-03-25 18:06:22 +0100

    use isNodeIncluded in soft-break plugin

--- packages/common/src/slate-plugins/soft-break/onKeyDown.ts
+++ packages/common/src/slate-plugins/soft-break/onKeyDown.ts
@@ -1,6 +1,6 @@
 import isHotkey from "is-hotkey"
 import { SoftBreakEditor, SoftBreakPluginOptions } from "./types"
-import { Block } from "../../slate-helpers"
+import { Block, isNodeIncluded } from "../../slate-helpers"
 
 export const onKeyDownSoftBreak = ({
   hotkey = "shift+Enter",
@@ -9,11 +9,8 @@ export const onKeyDownSoftBreak = ({
 }: SoftBreakPluginOptions = {}) => (event: KeyboardEvent, editor: SoftBreakEditor) => {
   if (isHotkey(hotkey, event)) {
     const [firstNode] = Block.first(editor)
-    const isExcluded = exclude && exclude.includes(firstNode.type)
-    const isNotIncluded = include && !include.includes(firstNode.type)
-
-    // If the type is explicity excluded or not included a default line break should be inserted
-    if (isExcluded || isNotIncluded) return
+    // If the type is explicity excluded or not included, a default line break should be inserted
+    if (!isNodeIncluded(firstNode,include,exclude)) return
     // prevent default which is an insertBreak command
     event.preventDefault()
     // insert soft break (defaults back to default insertBreak if type doesn't match)
