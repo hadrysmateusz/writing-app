576dbdbc83739b3e73b4a7291c5a5e926ada35ce
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-03-27 21:50:05 +0100

    an incomplete solution for deleting nodes around images
    this still needs work as it messes up selection on undo and doesn't cover all cases

--- packages/common/src/slate-plugins/elements/image/editorOverrides.ts
+++ packages/common/src/slate-plugins/elements/image/editorOverrides.ts
@@ -1,10 +1,61 @@
 import { EditorOverridesFactory, composeOverrides } from "@slate-plugin-system/core"
-import { withBreakInsertDefault } from "../../../slate-helpers"
+import {
+  withBreakInsertDefault,
+  Block,
+  isBlockTextEmpty,
+  isFirstChild,
+  isLastChild
+} from "../../../slate-helpers"
 import { isImageUrl, insertImage } from "./helpers"
 import { IMAGE } from "./types"
+import { Range, Node, Path, Transforms } from "slate"
 
 const withImageCore: EditorOverridesFactory = () => (editor) => {
-  const { insertData, isVoid } = editor
+  const { insertData, isVoid, deleteBackward, deleteForward } = editor
+
+  // TODO: consider preventing removing image nodes when they are not selected (this requires checking if the cursor is at the first/last position)
+
+  editor.deleteBackward = (unit) => {
+    if (editor.selection && Range.isCollapsed(editor.selection)) {
+      const [currentNode, currentPath] = Block.closest(editor)
+      if (!isFirstChild(currentPath)) {
+        const previousNode = Node.get(editor, Path.previous(currentPath))
+        if (
+          isBlockTextEmpty(currentNode) &&
+          currentNode.type !== IMAGE &&
+          previousNode.type === IMAGE
+        ) {
+          Transforms.removeNodes(editor)
+          return
+        }
+      }
+    }
+    deleteBackward(unit)
+  }
+
+  editor.deleteForward = (unit) => {
+    if (editor.selection && Range.isCollapsed(editor.selection)) {
+      const [currentNode, nextPath] = Block.closest(editor)
+      if (!isLastChild(editor, nextPath)) {
+        const nextNode = Node.get(editor, Path.next(nextPath))
+        if (
+          isBlockTextEmpty(currentNode) &&
+          currentNode.type !== IMAGE &&
+          nextNode.type === IMAGE
+        ) {
+          Transforms.removeNodes(editor)
+          // this causes history issues (undoing requires two steps into one)
+          // I believe this is the same issue that I have with move nodes -
+          // when undoing the selection operation is applied first and the node tree has changed so it gets applied incorrectly
+          // I could try to use more lower-level logic to make sure the selection stays consistent
+          // or I could investigate how slate-history works and implement some sort of batching
+          Transforms.select(editor, nextPath)
+          return
+        }
+      }
+    }
+    deleteForward(unit)
+  }
 
   editor.isVoid = (element) => {
     return element.type === IMAGE ? true : isVoid(element)
