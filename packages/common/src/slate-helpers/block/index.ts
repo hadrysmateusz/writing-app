65d4f4137feca3bc8c81c45fe88a854e2a319a91
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-03-27 21:48:11 +0100

    improve typing for Blocks

--- packages/common/src/slate-helpers/block/index.ts
+++ packages/common/src/slate-helpers/block/index.ts
@@ -1,4 +1,4 @@
-import { Editor, NodeEntry, Location, Range } from "slate"
+import { Editor, NodeEntry, Location, Range, Ancestor } from "slate"
 
 /**
  * The `Block` interface is a very specific kind of slate node.
@@ -12,7 +12,7 @@ export const Block = {
   /**
    * Get closest block node ancestor
    */
-  closest(editor, options: { at?: Location } = {}): NodeEntry {
+  closest(editor, options: { at?: Location } = {}): NodeEntry<Ancestor> {
     const nodeEntry = Editor.above(editor, {
       at: options.at,
       match: (n) => Editor.isBlock(editor, n)
@@ -24,7 +24,7 @@ export const Block = {
   /**
    * Get first block at given location
    */
-  first(editor, options: { at?: Range } = {}): NodeEntry {
+  first(editor, options: { at?: Range } = {}): NodeEntry<Ancestor> {
     const { at = editor.selection } = options
     const startPoint = Range.start(at)
     return Block.closest(editor, { at: startPoint })
@@ -33,7 +33,7 @@ export const Block = {
   /**
    * Get last block at given location
    */
-  last(editor, options: { at?: Range } = {}): NodeEntry {
+  last(editor, options: { at?: Range } = {}): NodeEntry<Ancestor> {
     const { at = editor.selection } = options
     const endPoint = Range.end(at)
     return Block.closest(editor, { at: endPoint })
