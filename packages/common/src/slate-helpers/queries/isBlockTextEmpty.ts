2f6cbc6503de8bbf3e5d436c32801ae86b6ba31e
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-03-27 21:48:50 +0100

    add a todo comment to isBlockTextEmpty

--- packages/common/src/slate-helpers/queries/isBlockTextEmpty.ts
+++ packages/common/src/slate-helpers/queries/isBlockTextEmpty.ts
@@ -2,6 +2,7 @@ import { Ancestor } from "slate"
 
 /**
  * Is the node's leaf node's text content empty
+ * TODO: this might return a false positive when a node has some marks
  */
 export const isBlockTextEmpty = (node: Ancestor) =>
 	node.children && node.children[node.children.length - 1]?.text?.length === 0
