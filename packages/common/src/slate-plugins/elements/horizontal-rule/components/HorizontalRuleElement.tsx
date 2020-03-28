f4b87c213dd9cca9ced2670caeda3db5fa905992
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-03-28 11:58:24 +0100

    fix horizontal_rule rendering

--- packages/common/src/slate-plugins/elements/horizontal-rule/components/HorizontalRuleElement.tsx
+++ packages/common/src/slate-plugins/elements/horizontal-rule/components/HorizontalRuleElement.tsx
@@ -1,11 +1,33 @@
 import React from "react"
-import { RenderElementProps } from "slate-react"
+import { RenderElementProps, useSelected, useFocused } from "slate-react"
 import styled from "styled-components/macro"
 
 import { HORIZONTAL_RULE } from "../types"
 
-const StyledHR = styled.hr``
+const HRContainer = styled.div`
+margin: 14px 0;
+  padding: 16px 8px;
+  cursor: pointer;
+`
 
-export const HorizontalRuleElement = ({ attributes, children }: RenderElementProps) => (
-	<StyledHR {...attributes} data-slate-type={HORIZONTAL_RULE} />
-)
+const HR = styled.div<{ selected: boolean; focused: boolean }>`
+
+  height: 2px;
+  background: #efefef;
+  box-shadow: ${(props) =>
+    props.selected && props.focused ? "0 0 0 3px #B4D5FF" : "none"};
+`
+
+export const HorizontalRuleElement = ({ attributes, children }: RenderElementProps) => {
+  const selected = useSelected()
+  const focused = useFocused()
+
+  return (
+    <HRContainer {...attributes} data-slate-type={HORIZONTAL_RULE}>
+      <div contentEditable={false}>
+        <HR selected={selected} focused={focused} />
+      </div>
+      {children}
+    </HRContainer>
+  )
+}
