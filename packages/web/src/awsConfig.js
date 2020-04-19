06c0fbf91e53cf10d9f37f234706ec963253b2d8
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-04-19 15:43:11 +0200

    add amplify to react app

---
+++ packages/web/src/awsConfig.js
@@ -0,0 +1,12 @@
+export default {
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://2misc0p76f.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_9TVp2paNs",
+    APP_CLIENT_ID: "68jl4vur9i94v1217ifpjaoch7",
+    IDENTITY_POOL_ID: "us-east-1:33fd4323-b35c-4243-ac1f-34165b9276f9"
+  }
+};
--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,13 +1,35 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "typeface-ibm-plex-mono"
+import { Amplify } from "aws-amplify"
 
 import App from "Components/App"
 import "./index.css"
 import * as serviceWorker from "./serviceWorker"
 import config from "./config"
+import awsConfig from "./awsConfig"
 
 import { loadDevTools, loadConfig } from "@writing-tool/core"
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: awsConfig.cognito.REGION,
+    userPoolId: awsConfig.cognito.USER_POOL_ID,
+    identityPoolId: awsConfig.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: awsConfig.cognito.APP_CLIENT_ID,
+  },
+
+  API: {
+    endpoints: [
+      {
+        name: "documents",
+        endpoint: awsConfig.apiGateway.URL,
+        region: awsConfig.apiGateway.REGION,
+      },
+    ],
+  },
+})
 
 // load and install the dev tools (if they need to be)
 // and when that's done, let's render the app
@@ -15,9 +37,9 @@ import { loadDevTools, loadConfig } from "@writing-tool/core"
 // is called synchronously so there's no penalty for including this
 // in production.
 loadConfig(config, () =>
-	loadDevTools(() => {
-		ReactDOM.render(<App />, document.getElementById("root"))
-	})
+  loadDevTools(() => {
+    ReactDOM.render(<App />, document.getElementById("root"))
+  })
 )
 
 // If you want your app to work offline and load faster, you can change
