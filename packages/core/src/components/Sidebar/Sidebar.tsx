679c4e86f51bd8b941ddd36e45fba1290077d5e1
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-04-23 15:44:10 +0200

    add a basic document title list to the sidebar

--- packages/core/src/components/Sidebar/Sidebar.tsx
+++ packages/core/src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
+import { useAsyncEffect } from "../../hooks"
+import { API } from "aws-amplify"
 
 export const Sidebar: React.FC = ({ children }) => {
-  return <div>{children}</div>
+  // TODO: replace any type with the proper document object interface
+  const [documents, setDocuments] = useState<any[]>([])
+  const [error, setError] = useState<string | null>(null)
+
+  useAsyncEffect(async () => {
+    // TODO: handle unauthenticated calls
+
+    try {
+      // TODO: check if an error is thrown instead of returned when the status of response is 4xx or 5xx
+      const { data, error } = await API.get("documents", "/documents", undefined)
+      if (error) {
+        setError(error)
+        return
+      }
+
+      setDocuments(data)
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+      setError(error.message)
+    }
+  }, [])
+
+  return (
+    <div>
+      <div>{children}</div>
+      <div>{error ?? documents.map((doc) => <div>{doc.title}</div>)}</div>
+    </div>
+  )
 }
