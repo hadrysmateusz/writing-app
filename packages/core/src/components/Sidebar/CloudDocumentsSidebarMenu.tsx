eaff83a4a5dab3a2e1d06428bb4c6e01e1e7f8de
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-05-18 15:16:31 +0200

    move CloudDocumentsSidebarMenu to serparate file

---
+++ packages/core/src/components/Sidebar/CloudDocumentsSidebarMenu.tsx
@@ -0,0 +1,66 @@
+import React from "react"
+import styled from "styled-components/macro"
+import { Node } from "slate"
+
+import SidebarDocumentItem from "./SidebarDocumentItem"
+import { DocumentDoc } from "../Database"
+
+export const CloudDocumentsSidebarMenu: React.FC<{
+  documents: DocumentDoc[]
+  currentDocument: DocumentDoc | null
+  isCurrentModified: boolean
+  editorContent: Node[]
+  switchEditor: (documentId: string | null) => void
+  newDocument: (shouldSwitch?: boolean) => Promise<DocumentDoc | null>
+}> = ({
+  documents,
+  currentDocument,
+  isCurrentModified,
+  editorContent,
+  switchEditor,
+  newDocument,
+}) => {
+  const handleCreateDocument = async () => {
+    newDocument()
+  }
+
+  return (
+    <List>
+      {documents.map((doc) => {
+        const isCurrent = !!currentDocument && currentDocument.id === doc.id
+        const isModified = isCurrent && isCurrentModified
+        return (
+          <SidebarDocumentItem
+            key={doc.id}
+            document={doc}
+            editorContent={editorContent}
+            isCurrent={isCurrent}
+            isModified={isModified}
+            switchEditor={switchEditor}
+          />
+        )
+      })}
+      <div>
+        <NewButton onClick={handleCreateDocument}>+ Create New</NewButton>
+      </div>
+    </List>
+  )
+}
+
+const List = styled.div`
+  padding: 0 16px;
+  font-size: 12px;
+`
+
+const NewButton = styled.div`
+  user-select: none;
+  background: none;
+  border: none;
+  padding: 6px 0;
+  display: block;
+  cursor: pointer;
+  color: #676c72;
+  :hover {
+    color: #afb3b6;
+  }
+`
--- packages/core/src/components/Sidebar/Sidebar.tsx
+++ packages/core/src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,11 @@
 import React, { useState, ChangeEvent } from "react"
 import styled from "styled-components/macro"
+import { Node } from "slate"
 
+import { CloudDocumentsSidebarMenu } from "./CloudDocumentsSidebarMenu"
 import { LogoutButton } from "../LogoutButton"
 import { ConnectWithMedium } from "../ConnectWithMedium"
-import SidebarDocumentItem from "./SidebarDocumentItem"
 import { DocumentDoc } from "../Database"
-import { Node } from "slate"
 
 // TODO: maybe replace with an enum or something to be able to use this type with the onChange event
 type MenuType = "CLOUD_DOCUMENTS" | "LOCAL_DOCUMENTS" | "AUTH"
@@ -32,7 +32,7 @@ export const Sidebar: React.FC<{
     switch (menuType) {
       case "CLOUD_DOCUMENTS":
         return (
-          <CloudDocumentsSidebar
+          <CloudDocumentsSidebarMenu
             documents={documents}
             currentDocument={currentDocument}
             isCurrentModified={isCurrentModified}
@@ -73,48 +73,6 @@ export const Sidebar: React.FC<{
   )
 }
 
-const CloudDocumentsSidebar: React.FC<{
-  documents: DocumentDoc[]
-  currentDocument: DocumentDoc | null
-  isCurrentModified: boolean
-  editorContent: Node[]
-  switchEditor: (documentId: string | null) => void
-  newDocument: (shouldSwitch?: boolean) => Promise<DocumentDoc | null>
-}> = ({
-  documents,
-  currentDocument,
-  isCurrentModified,
-  editorContent,
-  switchEditor,
-  newDocument,
-}) => {
-  const handleCreateDocument = async () => {
-    newDocument()
-  }
-
-  return (
-    <List>
-      {documents.map((doc) => {
-        const isCurrent = !!currentDocument && currentDocument.id === doc.id
-        const isModified = isCurrent && isCurrentModified
-        return (
-          <SidebarDocumentItem
-            key={doc.id}
-            document={doc}
-            editorContent={editorContent}
-            isCurrent={isCurrent}
-            isModified={isModified}
-            switchEditor={switchEditor}
-          />
-        )
-      })}
-      <div>
-        <NewButton onClick={handleCreateDocument}>+ Create New</NewButton>
-      </div>
-    </List>
-  )
-}
-
 const HeaderSelect = styled.select`
   color: white;
   width: calc(100% - 16px);
@@ -130,24 +88,6 @@ const HeaderSelect = styled.select`
   > * {
     color: black;
     font-size: 14px;
-  }
-`
-
-const List = styled.div`
-  padding: 0 16px;
-  font-size: 12px;
-`
-
-const NewButton = styled.div`
-  user-select: none;
-  background: none;
-  border: none;
-  padding: 6px 0;
-  display: block;
-  cursor: pointer;
-  color: #676c72;
-  :hover {
-    color: #afb3b6;
   }
 `
 
