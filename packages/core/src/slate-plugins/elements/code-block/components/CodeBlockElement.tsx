410f6606b2ed9b58394cb287ea2a10f592fb9b8a
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-05-26 00:53:19 +0200

    update code block styling

--- packages/core/src/slate-plugins/elements/code-block/components/CodeBlockElement.tsx
+++ packages/core/src/slate-plugins/elements/code-block/components/CodeBlockElement.tsx
@@ -5,12 +5,16 @@ import styled from "styled-components/macro"
 import { CODE_BLOCK } from "../types"
 
 const StyledPre = styled.pre`
+  position: relative;
   font-family: monospace;
-  background-color: #f5f5f5;
-  color: #111;
-  padding: 8px;
+  background-color: #41474d;
+  color: #f3f3f3;
+  padding: 14px 18px;
+  font-size: 15px;
+  line-height: 24px;
   border-radius: 5px;
   white-space: pre-wrap;
+  border: 1px solid #1b1f23;
 `
 
 export const CodeBlockElement = ({
