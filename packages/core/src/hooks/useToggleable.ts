d7dfe55d9f2af320c42528ecf5204b254e033cec
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-06-11 13:38:43 +0200

    create useToggleable hook

---
+++ packages/core/src/hooks/useToggleable.ts
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+export interface Toggleable {
+  isOpen: boolean
+  open: () => void
+  close: () => void
+  toggle: () => void
+}
+
+export const useToggleable = (
+  initialState: boolean,
+  {
+    onBeforeOpen,
+    onAfterOpen,
+    onBeforeClose,
+    onAfterClose,
+  }: {
+    onBeforeOpen?: () => void
+    onAfterOpen?: () => void
+    onBeforeClose?: () => void
+    onAfterClose?: () => void
+  } = {}
+) => {
+  const [isOpen, setIsOpen] = useState(initialState)
+
+  const open = () => {
+    onBeforeOpen && onBeforeOpen()
+    setIsOpen(true)
+    onAfterOpen && onAfterOpen()
+  }
+
+  const close = () => {
+    onBeforeClose && onBeforeClose()
+    setIsOpen(false)
+    onAfterClose && onAfterClose()
+  }
+
+  const toggle = () => {
+    // The abstraction functions are used to make sure any and all pre-hooks are fired
+    if (isOpen) {
+      close()
+    } else {
+      open()
+    }
+  }
+
+  return { open, close, toggle, isOpen }
+}
