08d7ea6b4e063ac2b03f3cc95f070062dde6efbc
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-06-11 23:58:01 +0200

    add styling props to Icon component

--- packages/core/src/components/Icon/index.tsx
+++ packages/core/src/components/Icon/index.tsx
@@ -1,11 +1,17 @@
 import React from "react"
 import iconLibrary from "./library"
 
-const Icon: React.FC<{ icon: string }> = ({ icon, ...rest }) => {
+const Icon: React.FC<{
+  icon: string
+  color?: string
+  style?: React.CSSProperties
+}> = ({ icon, color, style = {}, ...rest }) => {
   const iconComponent = iconLibrary[icon]
   if (!iconComponent) console.error("invalid icon:", icon)
   return iconComponent ? (
-    <div {...rest}>{React.createElement(iconComponent, null, null)}</div>
+    <div style={{ ...style, color }} {...rest}>
+      {React.createElement(iconComponent, null, null)}
+    </div>
   ) : null
 }
 
