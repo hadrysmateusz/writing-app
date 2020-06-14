d6879291ab9d1204dbf513428db7ab7c9162f3fd
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-06-14 18:08:15 +0200

    change some default styles for the Icon component

--- packages/core/src/components/Icon/index.tsx
+++ packages/core/src/components/Icon/index.tsx
@@ -1,6 +1,14 @@
 import React from "react"
 import iconLibrary from "./library"
 
+const baseStyle = {
+  lineHeight: "1em",
+  fontSize: "1em",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}
+
 const Icon: React.FC<{
   icon: string
   color?: string
@@ -9,8 +17,13 @@ const Icon: React.FC<{
   const iconComponent = iconLibrary[icon]
   if (!iconComponent) console.error("invalid icon:", icon)
   return iconComponent ? (
-    <div style={{ ...style, color }} {...rest}>
-      {React.createElement(iconComponent, null, null)}
+    <div style={{ ...baseStyle, ...style, color }} {...rest}>
+      {React.createElement(
+        iconComponent,
+        // To counteract the default display: inline-block of svg
+        { style: { display: "block", paddingBottom: "1px" } },
+        null
+      )}
     </div>
   ) : null
 }
