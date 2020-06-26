97a0a30810c9f52e6ae9ce3e58bf8b2daf1f473c
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-06-26 22:52:13 +0200

    change formatOptional utility to handle undefined and null text

--- packages/core/src/utils/formatOptional.ts
+++ packages/core/src/utils/formatOptional.ts
@@ -1,8 +1,13 @@
 /**
- * Small utility to provide a placeholder for text values that might be empty
+ * Small utility to provide a placeholder for text values that might be empty or undefined
+ *
+ * It also checks if the trimmed string is empty to prevent visually empty strings
  */
 
-export const formatOptional = (text: string, placeholder: string) => {
-  const trimmedText = text.trim()
+export const formatOptional = (
+  text: string | undefined | null,
+  placeholder: string
+) => {
+  const trimmedText = text ? text.trim() : ""
   return trimmedText === "" ? placeholder : trimmedText
 }
