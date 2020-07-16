643580dc687aafbc31ae4b270d2984ee403ce9c7
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-07-17 01:18:50 +0200

    move trash banner to separate file

--- packages/core/src/components/Editor/Editor.tsx
+++ packages/core/src/components/Editor/Editor.tsx
@@ -3,7 +3,6 @@ import React, {
   useState,
   useRef,
   useEffect,
-  useCallback,
   MouseEvent,
 } from "react"
 import styled from "styled-components/macro"
@@ -27,6 +26,7 @@ import {
   InnerContainer,
 } from "./styledComponents"
 import { useDocumentsAPI } from "../DocumentsAPI"
+import TrashBanner from "../TrashBanner"
 import {
   ContextMenuItem,
   useContextMenu,
@@ -424,67 +424,6 @@ const EditorComponent: React.FC<{
     </OutermostContainer>
   )
 }
-
-const TrashBanner: React.FC<{ documentId: string }> = ({ documentId }) => {
-  const { restoreDocument, permanentlyRemoveDocument } = useDocumentsAPI()
-
-  const handleRestoreDocument = useCallback(() => {
-    restoreDocument(documentId)
-  }, [documentId, restoreDocument])
-
-  const handlePermanentlyRemoveDocument = useCallback(() => {
-    permanentlyRemoveDocument(documentId)
-  }, [documentId, permanentlyRemoveDocument])
-
-  return (
-    <TrashBannerContainer>
-      <div>This document is in Trash</div>
-      <button onClick={handleRestoreDocument}>Restore</button>
-      <button onClick={handlePermanentlyRemoveDocument}>
-        Delete permanently
-      </button>
-    </TrashBannerContainer>
-  )
-}
-
-export const TrashBannerContainer = styled.div`
-  background-color: #db4141;
-  height: 44px;
-  width: 100%;
-  color: white;
-  font-size: 14px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: default;
-  button {
-    cursor: pointer;
-    display: block;
-    border: 1px solid white;
-    background: transparent;
-    border-radius: 2px;
-    padding: 4px 14px;
-    font-family: "Poppins";
-    font-size: 12px;
-    font-weight: 500;
-    color: white;
-
-    transition: background-color 200ms ease;
-
-    :hover {
-      background-color: #e34d4d;
-    }
-
-    :active {
-      background-color: #d13b3b;
-    }
-
-    /* TODO: figure out focus / outline styles */
-  }
-  > * + * {
-    margin-left: 12px;
-  }
-`
 
 const StyledNamingInput = styled(NamingInput)`
   margin-top: 16px;
---
+++ packages/core/src/components/TrashBanner.tsx
@@ -0,0 +1,66 @@
+import React, { useCallback } from "react"
+import styled from "styled-components/macro"
+import { useDocumentsAPI } from "./DocumentsAPI"
+
+const TrashBanner: React.FC<{ documentId: string }> = ({ documentId }) => {
+  const { restoreDocument, permanentlyRemoveDocument } = useDocumentsAPI()
+
+  const handleRestoreDocument = useCallback(() => {
+    restoreDocument(documentId)
+  }, [documentId, restoreDocument])
+
+  const handlePermanentlyRemoveDocument = useCallback(() => {
+    permanentlyRemoveDocument(documentId)
+  }, [documentId, permanentlyRemoveDocument])
+
+  return (
+    <TrashBannerContainer>
+      <div>This document is in Trash</div>
+      <button onClick={handleRestoreDocument}>Restore</button>
+      <button onClick={handlePermanentlyRemoveDocument}>
+        Delete permanently
+      </button>
+    </TrashBannerContainer>
+  )
+}
+
+const TrashBannerContainer = styled.div`
+  background-color: #db4141;
+  height: 44px;
+  width: 100%;
+  color: white;
+  font-size: 14px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: default;
+  button {
+    cursor: pointer;
+    display: block;
+    border: 1px solid white;
+    background: transparent;
+    border-radius: 2px;
+    padding: 4px 14px;
+    font-family: "Poppins";
+    font-size: 12px;
+    font-weight: 500;
+    color: white;
+
+    transition: background-color 200ms ease;
+
+    :hover {
+      background-color: #e34d4d;
+    }
+
+    :active {
+      background-color: #d13b3b;
+    }
+
+    /* TODO: figure out focus / outline styles */
+  }
+  > * + * {
+    margin-left: 12px;
+  }
+`
+
+export default TrashBanner
