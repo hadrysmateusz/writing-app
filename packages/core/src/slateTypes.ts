b1bd8709ce46565d8db1abf03f928683f14d2a57
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-07-29 15:06:18 +0200

    add new constants for some slate types

--- packages/core/src/slateTypes.ts
+++ packages/core/src/slateTypes.ts
@@ -3,3 +3,8 @@ export enum ListType {
   UL_LIST = "bulleted_list",
   LIST_ITEM = "list_item",
 }
+
+export const ELEMENT_OL_LIST = ListType.OL_LIST as string
+export const ELEMENT_UL_LIST = ListType.UL_LIST as string
+export const ELEMENT_LIST_ITEM = ListType.LIST_ITEM as string
+export const ELEMENT_PARAGRAPH = "paragraph"
