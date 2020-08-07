22a36ce723ed1f5f08c96326309edc800176ce46
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-08-07 17:05:36 +0200

    start work on new modals

---
+++ packages/core/src/components/Modal/ModalContext.tsx
@@ -0,0 +1,6 @@
+import { useState } from "react"
+import { Editor, Transforms } from "slate"
+import { useSlate } from "slate-react"
+import createContext from "../../utils/createContext"
+
+createContext()
