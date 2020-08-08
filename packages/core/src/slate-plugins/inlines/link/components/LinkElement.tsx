76727a88d1cb7563fca04a705ca70311baa6ea30
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-08-09 00:50:30 +0200

    add styles to link element and escape the url

--- packages/core/src/slate-plugins/inlines/link/components/LinkElement.tsx
+++ packages/core/src/slate-plugins/inlines/link/components/LinkElement.tsx
@@ -1,14 +1,26 @@
 import React from "react"
 import { RenderElementProps } from "slate-react"
+import styled from "styled-components/macro"
+import escapeHtml from "escape-html"
 
 import { LINK } from "../types"
+
+const StyledA = styled.a`
+  color: #717171;
+`
 
 export const LinkElement = ({
   attributes,
   children,
   element,
-}: RenderElementProps) => (
-  <a {...attributes} data-slate-type={LINK} href={element.url}>
-    {children}
-  </a>
-)
+}: RenderElementProps) => {
+  const href = typeof element.url === "string" ? escapeHtml(element.url) : ""
+
+  // TODO: add a way to open the url in a browser with alt+click
+
+  return (
+    <StyledA {...attributes} data-slate-type={LINK} href={href}>
+      {children}
+    </StyledA>
+  )
+}
