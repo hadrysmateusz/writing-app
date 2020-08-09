d9895123f58fcecc5589a1d6c9ef70465ec9a451
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-08-09 02:22:26 +0200

    bring back custom list styles

--- packages/core/src/slate-plugins/elements/list/components/List.tsx
+++ packages/core/src/slate-plugins/elements/list/components/List.tsx
@@ -1,10 +1,17 @@
 import React from "react"
 import styled, { css } from "styled-components/macro"
 import { RenderElementProps } from "slate-react"
-import { ListType } from "../../../../slateTypes"
+import {
+  ListType,
+  ELEMENT_UL_LIST,
+  ELEMENT_PARAGRAPH,
+  ELEMENT_OL_LIST,
+} from "../../../../slateTypes"
 import { useListContext, ListContextProvider } from "../ListContext"
 import { ListItemContextProvider } from "../ListItemContext"
 import { config } from "../../../../dev-tools"
+
+// TODO: make ordered and unordered lists have the same effective indentation (Might require custom counters, and 3 digits is probably max)
 
 const listCommon = css`
   margin: 14px 0;
@@ -14,45 +21,63 @@ const listCommon = css`
     margin: 6px 0;
   }
 
-  li > * {
-    /* display: inline-block; */
+  li > *:first-child {
+    display: inline-block;
   }
 
-  padding-inline-start: 24px;
+  /* TODO: many more edge cases will probably be needed */
+  [data-slate-type="${ELEMENT_PARAGRAPH}"] + [data-slate-type="${ELEMENT_UL_LIST}"],
+  [data-slate-type="${ELEMENT_PARAGRAPH}"] + [data-slate-type="${ELEMENT_OL_LIST}"] {
+    /* TODO: base the margin on the actual size of the paragraph margin */
+    margin-top: -14px;
+  }
+
+  padding-inline-start: ${(p) => (p.noIndent ? "0" : "24px")};
   margin-block-start: 0;
   margin-block-end: 0;
 
   ${(p) =>
     p.debugStyles &&
-    `border: 1px dashed red;
-    background: rgba(255, 0, 0, 0.15);
-    padding: 4px;
-    padding-inline-start: 28px;`}
+    css`
+      border: 1px dashed red;
+      background: rgba(255, 0, 0, 0.15);
+      padding: 4px;
+      padding-inline-start: 28px;
+    `}
 `
 
-const StyledUL = styled.ul`
-  /* list-style-type: none; */
-  /* li::before {
+const StyledUL = styled.ul<{ debugStyles: boolean }>`
+  list-style-type: none;
+  li {
+    position: relative;
+  }
+  li::before {
+    position: absolute;
+    /* TODO: find a better way to find a top offset that responds properly to changes */
+    top: 11px;
+    left: -18px;
     content: "";
     color: #41474d;
-    display: inline-block;
+    display: block;
     background: #41474d;
     height: 6px;
     width: 6px;
     border-radius: 50%;
-    margin-bottom: 3px;
-    margin-right: 12px;
-    margin-left: -1px;
-  } */
+  }
   ${listCommon}
 `
 
-const StyledOL = styled.ol`
+const StyledOL = styled.ol<{ debugStyles: boolean }>`
   list-style-position: inside;
+  /* ::marker is supported in chromium 86+*/
+  li::marker {
+    color: #717171;
+    font-weight: 600;
+  }
   ${listCommon}
 `
 
-const StyledLI = styled.li`
+const StyledLI = styled.li<{ debugStyles: boolean }>`
   ${(p) =>
     p.debugStyles &&
     `border: 1px dashed green;
@@ -68,6 +93,7 @@ export const OL = ({ attributes, children }: RenderElementProps) => {
       {...attributes}
       data-slate-type={ListType.OL_LIST}
       debugStyles={config.debugStyles}
+      noIndent={listLevel === 0}
     >
       <ListContextProvider value={{ listLevel: listLevel + 1 }}>
         {children}
