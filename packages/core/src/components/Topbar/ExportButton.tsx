7403f92ad378f88d8f63867ae91df352c6611ae7
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-08-09 03:13:24 +0200

    add modal to export button

--- packages/core/src/components/Topbar/ExportButton.tsx
+++ packages/core/src/components/Topbar/ExportButton.tsx
@@ -2,15 +2,62 @@ import React from "react"
 import { useSlate } from "slate-react"
 
 import { Button } from "../Button"
-import { serializeHTML } from "../../slate-helpers"
+import { serializeHTML, parseToMarkdown } from "../../slate-helpers"
+import { useModal } from "../Modal"
+import styled from "styled-components/macro"
 
-export const ExportButton: React.FC = () => {
+const ModalContainer = styled.div`
+  background: #252525;
+  border: 1px solid #363636;
+  padding: 20px;
+  border-radius: 4px;
+  color: white;
+`
+
+export const ExportModalContent: React.FC<{
+  close: () => void
+}> = ({ close }) => {
   const editor = useSlate()
 
-  const handleExport = () => {
-    const html = serializeHTML(editor)
-    console.log(html)
+  const handleExportHTML = () => {
+    const exported = serializeHTML(editor)
+    alert(exported)
+    close()
   }
 
-  return <Button onClick={handleExport}>Export</Button>
+  const handleExportMarkdown = () => {
+    const exported = editor.children
+      .map((node) => parseToMarkdown(node))
+      .join("\n")
+    alert(exported)
+    close()
+  }
+
+  return (
+    <ModalContainer>
+      <Button onClick={handleExportMarkdown}>Export as Markdown</Button>
+      <Button onClick={handleExportHTML}>Export as HTML</Button>
+    </ModalContainer>
+  )
+}
+
+export const ExportButton: React.FC = () => {
+  const {
+    open: openExportModal,
+    close: closeExportModal,
+    Modal: ExportModal,
+  } = useModal(false)
+
+  const handleExport = () => {
+    openExportModal()
+  }
+
+  return (
+    <>
+      <Button onClick={handleExport}>Export</Button>
+      <ExportModal>
+        <ExportModalContent close={closeExportModal} />
+      </ExportModal>
+    </>
+  )
 }
