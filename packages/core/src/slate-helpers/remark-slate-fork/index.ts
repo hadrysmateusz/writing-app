96266cb1b83433cee2b13b9b34ef68e499dbab4c
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-08-13 23:28:03 +0200

    add a modified version of remark-slate

---
+++ packages/core/src/slate-helpers/remark-slate-fork/index.ts
@@ -0,0 +1,7 @@
+import deserialize, { defaultNodeTypes } from "./deserialize"
+import serialize from "./serialize"
+import plugin from "./plugin"
+
+export { deserialize, serialize, defaultNodeTypes }
+
+export default plugin
