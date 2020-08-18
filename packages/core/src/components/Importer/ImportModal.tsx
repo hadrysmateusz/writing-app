f5b73f038a9655d6a596b83572460cc46e252b37
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-08-18 02:38:02 +0200

    refactor the importer + open the imported document and import location

--- packages/core/src/components/Importer/ImportModal.tsx
+++ packages/core/src/components/Importer/ImportModal.tsx
@@ -1,10 +1,12 @@
 import React, { useCallback } from "react"
+import styled from "styled-components/macro"
 
 import { Button } from "../Button"
-import styled from "styled-components/macro"
 import { useDocumentsAPI } from "../MainProvider"
 import { deserializeMarkdown } from "../../slate-helpers/deserialize"
-import { Node } from "slate"
+import { useViewState } from "../View"
+import { useMainState } from "../MainProvider"
+import { VIEWS } from "../Sidebar/types"
 
 const ModalContainer = styled.div`
   background: #252525;
@@ -32,13 +34,52 @@ export const ImportModalContent: React.FC<{
   close: () => void
 }> = ({ close }) => {
   const { createDocument } = useDocumentsAPI()
+  const { primarySidebar } = useViewState()
+  const { switchDocument } = useMainState()
+
+  const handleSuccess = useCallback(
+    async (result: any, format: "md") => {
+      const deserializer = { md: deserializeMarkdown }[format]
+
+      const files = result.data as { fileName: string; content: string }[]
+
+      // TODO: add some validation, and make sure importing files can't be exploited
+
+      const parsed = files.map((file) => {
+        const { fileName, content } = file
+        return {
+          title: fileName,
+          content: deserializer(content),
+        }
+      })
+
+      const documentPromises = parsed.map(({ title, content }) => {
+        // If I switch to using the first heading as title, then inferring the title might not be necessary (although if there is no first heading then I should probably use the file name)
+        return createDocument(null, { title, content }, { switchTo: false })
+      })
+
+      const documents = await Promise.all(documentPromises)
+
+      const num = documents.length
+
+      if (num === 0) {
+        console.error(
+          "Importer returned an unexpected result: no files with status success"
+        )
+        return
+      }
+
+      const docId = documents[0].id
+      switchDocument(docId)
+
+      // TODO: when selecting target collection for imports is implemented this should be replaced with the groupId
+      primarySidebar.switchView(VIEWS.INBOX)
+    },
+    [createDocument, primarySidebar, switchDocument]
+  )
 
   const importFile = useCallback(
-    async (format: "html" | "md" /* TODO: replace with FileFormats enum */) => {
-      const deserializer = { md: deserializeMarkdown }[format] as (
-        content: string
-      ) => Node[]
-
+    async (format: "md") => {
       const result = await window.ipcRenderer.invoke("read-file", {
         format: format,
       })
@@ -49,31 +90,19 @@ export const ImportModalContent: React.FC<{
       }
 
       if (result.status === "success") {
-        const files = result.data as { fileName: string; content: string }[]
-
-        const parsed = files.map((file) => {
-          const { fileName, content } = file
-          return {
-            title: fileName,
-            content: deserializer(content),
-          }
-        })
-
-        parsed.forEach(({ title, content }) => {
-          // If I switch to using the first heading as title, then inferring the title might not be necessary (although if there is no first heading then I should probably use the file name)
-          createDocument(null, { title, content }, { switchTo: false })
-        })
+        handleSuccess(result, format)
       }
 
       close()
     },
-    [close, createDocument]
+    [close, handleSuccess]
   )
 
   const handleImportMarkdown = useCallback(() => {
     importFile("md")
   }, [importFile])
 
+  // TODO: add a selector for the target collection
   return (
     <ModalContainer>
       <h2>Import</h2>
