895fb94bcbc64deca7a748e8634d58c9c93916b1
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-09-28 19:18:09 +0200

    add comment to plain text serializer

--- packages/core/src/slate-helpers/serialize/text.ts
+++ packages/core/src/slate-helpers/serialize/text.ts
@@ -2,6 +2,7 @@ import { Node } from "slate"
 
 // TODO: support lists (newlines and indentation)
 // TODO: consider not adding empty lines where images should be (or some other custom handling)
+// TODO: doesn't properly add new lines in nested nodes like lists
 export const serializeText = (nodes: Node[]) => {
   return nodes.map((n) => Node.string(n)).join("\n")
 }
