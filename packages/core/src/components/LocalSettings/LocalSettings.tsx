48f65b8c6b6c46de2f9ce8a067e6e007c5990593
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-10-04 23:48:10 +0200

    optimistically update local settings state

--- packages/core/src/components/LocalSettings/LocalSettings.tsx
+++ packages/core/src/components/LocalSettings/LocalSettings.tsx
@@ -23,14 +23,6 @@ export const LocalSettingsProvider: React.FC = ({ children }) => {
   const [isInitialLoad, setIsInitialLoad] = useState(() => true)
 
   /**
-   * RxDB document containing the local settings
-   */
-  const [
-    localSettingsDoc,
-    setLocalSettingsDoc,
-  ] = useState<LocalSettingsDoc | null>(null)
-
-  /**
    * RxDB query that fetches the local settings
    */
   const query = useMemo(() => db.local_settings.findOne(currentUser.username), [
@@ -38,9 +30,18 @@ export const LocalSettingsProvider: React.FC = ({ children }) => {
     db.local_settings,
   ])
 
-  //#region values of the actual local settings - separated to optimize rerendering
+  /**
+   * RxDB document containing the local settings
+   */
+  const [
+    localSettingsDoc,
+    setLocalSettingsDoc,
+  ] = useState<LocalSettingsDoc | null>(null)
 
-  const [expandedKeys, setIsSpellCheckEnabled] = useState<
+  //#region values of the actual local settings - separated to optimize rerendering
+  // TODO: I shouldn't have to separate this - INVESTIGATE
+
+  const [expandedKeys, setExpandedKeys] = useState<
     LocalSettings["expandedKeys"]
   >(defaults.expandedKeys)
 
@@ -73,75 +74,70 @@ export const LocalSettingsProvider: React.FC = ({ children }) => {
 
   //#endregion
 
-  const updateInternalState = useCallback(
-    (newLocalSettingsDoc: LocalSettingsDoc | null) => {
-      const data = newLocalSettingsDoc?.toJSON()
-
-      /**
-       * Updates internal state for a given setting if it's present on the fetched document
-       *
-       * TODO: better handle the case where the value is not present because that indicates an issue (maybe use the default, or even update the RxDB collection with the default)
-       */
-      const update = <K extends keyof LocalSettings>(
-        key: K,
-        updater: React.Dispatch<React.SetStateAction<LocalSettings[K]>>
-      ) => {
-        const value = data?.[key]
-        if (value === undefined) return
-        updater(value)
-      }
-
-      setLocalSettingsDoc(newLocalSettingsDoc)
-
-      //#region update all the values of settings in state
-
-      update("expandedKeys", setIsSpellCheckEnabled)
-      update("primarySidebarCurrentView", setPrimarySidebarCurrentView)
-      update("secondarySidebarCurrentView", setSecondarySidebarCurrentView)
-      update("currentEditor", setCurrentEditor)
-      update("primarySidebarIsOpen", setPrimarySidebarIsOpen)
-      update("secondarySidebarIsOpen", setSecondarySidebarIsOpen)
-      update("navigatorSidebarIsOpen", setNavigatorSidebarIsOpen)
-
-      //#endregion
-    },
-    []
-  )
-
   // Get the local settings for the first time
   useEffect(() => {
-    if (isInitialLoad) {
-      query.exec().then((newLocalSettingsDoc) => {
-        if (!newLocalSettingsDoc) {
-          db.local_settings.insert({
-            ...defaults,
-            userId: currentUser.username,
-          })
-        }
+    console.log(isInitialLoad, currentUser.username)
 
-        updateInternalState(newLocalSettingsDoc)
-        setIsInitialLoad(false)
-      })
+    if (isInitialLoad) {
+      db.local_settings
+        .findOne(currentUser.username)
+        .exec()
+        .then((newLocalSettingsDoc) => {
+          if (!newLocalSettingsDoc) {
+            db.local_settings.insert({
+              ...defaults,
+              userId: currentUser.username,
+            })
+          }
+
+          const data = newLocalSettingsDoc?.toJSON()
+
+          /**
+           * Updates internal state for a given setting if it's present on the fetched document
+           *
+           * TODO: better handle the case where the value is not present because that indicates an issue (maybe use the default, or even update the RxDB collection with the default)
+           */
+          const update = <K extends keyof LocalSettings>(
+            key: K,
+            updater: React.Dispatch<React.SetStateAction<LocalSettings[K]>>
+          ) => {
+            const value = data?.[key]
+            if (value === undefined) return
+            updater(value)
+          }
+
+          console.log("newLocalSettingsDoc", newLocalSettingsDoc)
+
+          setLocalSettingsDoc(newLocalSettingsDoc)
+
+          //#region update all the values of settings in state
+
+          update("expandedKeys", setExpandedKeys)
+          update("primarySidebarCurrentView", setPrimarySidebarCurrentView)
+          update("secondarySidebarCurrentView", setSecondarySidebarCurrentView)
+          update("currentEditor", setCurrentEditor)
+          update("primarySidebarIsOpen", setPrimarySidebarIsOpen)
+          update("secondarySidebarIsOpen", setSecondarySidebarIsOpen)
+          update("navigatorSidebarIsOpen", setNavigatorSidebarIsOpen)
+
+          //#endregion
+
+          setIsInitialLoad(false)
+        })
     }
-  }, [
-    currentUser.username,
-    db.local_settings,
-    isInitialLoad,
-    query,
-    updateInternalState,
-  ])
+  }, [currentUser.username, db.local_settings, isInitialLoad])
 
   // Set up local settings subscription
   useEffect(() => {
     const subscription = query.$.subscribe((newLocalSettingsDoc) => {
       console.log(newLocalSettingsDoc)
-      updateInternalState(newLocalSettingsDoc)
+      setLocalSettingsDoc(newLocalSettingsDoc)
     })
 
     return () => {
       subscription.unsubscribe()
     }
-  }, [query.$, updateInternalState])
+  }, [query.$])
 
   /**
    * Updates a setting under the given key with the given value
@@ -151,22 +147,50 @@ export const LocalSettingsProvider: React.FC = ({ children }) => {
    *
    * @todo this typing doesn't support nested properties
    */
-  const updateLocalSetting = async <K extends keyof LocalSettings>(
-    key: K,
-    value: LocalSettings[K]
-  ) => {
-    if (localSettingsDoc === null) {
-      throw Error("The localSettingsDoc object is missing")
-    }
+  const updateLocalSetting = useCallback(
+    async <K extends keyof LocalSettings>(key: K, value: LocalSettings[K]) => {
+      // Optimistically updates the state
+      switch (key) {
+        case "expandedKeys":
+          setExpandedKeys(value)
+          break
+        case "primarySidebarCurrentView":
+          setPrimarySidebarCurrentView(value)
+          break
+        case "secondarySidebarCurrentView":
+          setSecondarySidebarCurrentView(value)
+          break
+        case "currentEditor":
+          setCurrentEditor(value)
+          break
+        case "primarySidebarIsOpen":
+          setPrimarySidebarIsOpen(value)
+          break
+        case "secondarySidebarIsOpen":
+          setSecondarySidebarIsOpen(value)
+          break
+        case "navigatorSidebarIsOpen":
+          setNavigatorSidebarIsOpen(value)
+          break
+        default:
+          throw new Error(`unknown setting ${key}`)
+      }
 
-    try {
-      const newDoc = await localSettingsDoc.atomicSet(key, value)
-      return newDoc
-    } catch (error) {
-      // TODO: better error handling
-      throw error
-    }
-  }
+      if (localSettingsDoc === null) {
+        throw Error("The localSettingsDoc object is missing")
+      }
+
+      try {
+        const newDoc = await localSettingsDoc.atomicSet(key, value)
+        return newDoc
+      } catch (error) {
+        // TODO: better error handling
+        console.error("Local settings change wasn't saved")
+        throw error
+      }
+    },
+    [localSettingsDoc]
+  )
 
   return (
     <LocalSettingsContext.Provider
