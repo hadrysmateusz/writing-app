e087660b219bbdfba64ea49f8c1dc3344588d997
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-10-06 05:54:09 +0200

    change types of the applyPlugins utility

--- packages/core/src/slate-plugin-system/utilities/applyPlugins.ts
+++ packages/core/src/slate-plugin-system/utilities/applyPlugins.ts
@@ -1,12 +1,12 @@
 import { Editor } from "slate"
-import { SlatePlugin } from "types"
+import { SlatePlugin } from "../types"
 
 // TODO: the combineReducers function in @reduxjs/toolkit might provide some information on how to make the returned object contain all of the applied types
 
-export const applyPlugins = (
-  editor: Editor,
+export const applyPlugins = <T extends Editor>(
+  editor: T,
   plugins: SlatePlugin[]
-): Editor => {
+): T => {
   // we reverse the array to execute functions from right to left
   plugins.reverse().forEach((plugin) => {
     if (plugin.editorOverrides) {
