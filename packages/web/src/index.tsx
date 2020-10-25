3c132394b882dfe773224dc7504c22090613c7a0
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-10-26 00:47:11 +0100

    update react, cra, and typescript

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -9,8 +9,10 @@ import { loadDevTools, loadConfig } from "./dev-tools"
 import { App } from "./components"
 
 import "./index.css"
-import * as serviceWorker from "./serviceWorker"
 import config from "./config"
+
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
+import reportWebVitals from "./reportWebVitals"
 
 // load and install the dev tools (if they need to be)
 // and when that's done, let's render the app
@@ -19,11 +21,21 @@ import config from "./config"
 // in production.
 loadConfig(config, () =>
   loadDevTools(() => {
-    ReactDOM.render(<App />, document.getElementById("root"))
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById("root")
+    )
   })
 )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
