308cf22467b4633986462f692046fa9df28f4ce8
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2021-09-23 05:15:18 +0200

    make slate use the new typing system

--- packages/web/src/components/EditorStateProvider/EditorStateProvider.tsx
+++ packages/web/src/components/EditorStateProvider/EditorStateProvider.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useState, useMemo } from "react"
 import { isEqual } from "lodash"
 import { createEditor, Descendant, BaseEditor } from "slate"
-import { ReactEditor, Slate } from "slate-react"
+import { ReactEditor, Slate, withReact } from "slate-react"
 
 import { SaveDocumentFn, useMainState } from "../MainProvider"
 import { deserialize, serialize } from "../Editor"
@@ -9,11 +9,29 @@ import { ImageModalProvider } from "../ImageModal"
 import { LinkModalProvider } from "../LinkPrompt"
 import { DEFAULT_EDITOR_VALUE, EditorState } from "../Main"
 
-import { applyPlugins } from "../../slate-plugin-system"
+// import { applyPlugins } from "../../slate-plugin-system"
 import { useDevUtils } from "../../dev-tools"
-import { plugins } from "../../pluginsList"
+// import { plugins } from "../../pluginsList"
 import { withDelayRender } from "../../withDelayRender"
 import { createContext } from "../../utils"
+
+type ImageElement = {
+  type: "image"
+  url: string
+  children: (CustomText | CustomElement)[]
+}
+type GenericElement = { type: string; children: (CustomText | CustomElement)[] }
+type CustomElement = GenericElement | ImageElement
+type CustomText = { text: string }
+type CustomEditor = BaseEditor & ReactEditor & { type: string | undefined }
+
+declare module "slate" {
+  interface CustomTypes {
+    Editor: CustomEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
 
 const NoEditorState = withDelayRender(1000)(() => <div>No editor object</div>)
 
@@ -40,10 +58,11 @@ export const EditorStateProvider: React.FC = ({ children }) => {
   }, [currentDocument])
 
   // If the editor needs to be accessed above in the react tree, try using some kind of pub/sub / event system. Don't lift this because it will have a huge performance impact
-  const [editor, setEditor] = useState<ReactEditor | null>(null)
+  const [editor, setEditor] = useState<CustomEditor | null>(null)
 
   const createEditorObject = useCallback(() => {
-    let editor = applyPlugins(createEditor(), plugins) as ReactEditor
+    // let editor = applyPlugins(createEditor(), plugins) as ReactEditor
+    let editor = withReact(createEditor())
     setEditor(editor)
   }, [])
 
