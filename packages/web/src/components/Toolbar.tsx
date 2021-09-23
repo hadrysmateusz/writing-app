7f36bf730164892f92aae8fa1f4edbf809714815
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2021-09-23 04:34:17 +0200

    comment out the currently unused Toolbar component

--- packages/web/src/components/Toolbar.tsx
+++ packages/web/src/components/Toolbar.tsx
@@ -1,104 +1,107 @@
-import React, { useCallback, useMemo } from "react"
-import { useSlateStatic } from "slate-react"
-import styled from "styled-components/macro"
+export {}
 
-import {
-  BLOCKQUOTE,
-  CODE_BLOCK,
-  HeadingType,
-  toggleList,
-  insertHorizontalRule,
-  HORIZONTAL_RULE,
-  IMAGE,
-} from "../slate-plugins"
-import { ListType } from "../slateTypes"
-import FormatButton from "./FormatButton"
-import { useImageModal } from "./ImageModal"
+// import React, { useCallback, useMemo } from "react"
+// import { useSlateStatic } from "slate-react"
+// import styled from "styled-components/macro"
 
-const ToolbarContainer = styled.div`
-  color: "#afb3b6";
-`
+// import {
+//   BLOCKQUOTE,
+//   CODE_BLOCK,
+//   HeadingType,
+//   toggleList,
+//   insertHorizontalRule,
+//   HORIZONTAL_RULE,
+//   IMAGE,
+// } from "../slate-plugins"
+// import { ListType } from "../slateTypes"
+// import FormatButton from "./FormatButton"
+// import { useImageModal } from "./ImageModal"
 
-const Section = styled.div`
-  display: inline-block;
-  &:not(:last-child) {
-    margin-right: 20px;
-  }
-`
+// const ToolbarContainer = styled.div`
+//   color: "#afb3b6";
+// `
 
-export const Toolbar = () => {
-  const editor = useSlateStatic()
+// const Section = styled.div`
+//   display: inline-block;
+//   &:not(:last-child) {
+//     margin-right: 20px;
+//   }
+// `
 
-  const onInsertHorizontalRule = useCallback(
-    (event: React.MouseEvent) => {
-      event.preventDefault()
-      insertHorizontalRule(editor)
-    },
-    [editor]
-  )
+// export const Toolbar = () => {
+//   const editor = useSlateStatic()
+//   const { open: openImageModal } = useImageModal()
 
-  const onInsertImage = useCallback(
-    (event: React.MouseEvent) => {
-      event.preventDefault()
-      openImageModal()
-    },
-    [openImageModal]
-  )
+//   const onInsertHorizontalRule = useCallback(
+//     (event: React.MouseEvent) => {
+//       event.preventDefault()
+//       insertHorizontalRule(editor)
+//     },
+//     [editor]
+//   )
 
-  // TODO: ListType should eventually be replaced by a type without the list item
-  const onToggleList = useCallback(
-    (type: ListType) => (event: React.MouseEvent) => {
-      event.preventDefault()
-      toggleList(editor, type)
-    },
-    [editor]
-  )
+//   const onInsertImage = useCallback(
+//     (event: React.MouseEvent) => {
+//       event.preventDefault()
+//       openImageModal()
+//     },
+//     [openImageModal]
+//   )
 
-  const onToggleNumberedList = useMemo(() => onToggleList(ListType.OL_LIST), [
-    onToggleList,
-  ])
+//   // TODO: ListType should eventually be replaced by a type without the list item
+//   const onToggleList = useCallback(
+//     (type: ListType) => (event: React.MouseEvent) => {
+//       event.preventDefault()
+//       toggleList(editor, type)
+//     },
+//     [editor]
+//   )
 
-  const onToggleBulletedList = useMemo(() => onToggleList(ListType.UL_LIST), [
-    onToggleList,
-  ])
+//   const onToggleNumberedList = useMemo(() => onToggleList(ListType.OL_LIST), [
+//     onToggleList,
+//   ])
 
-  return (
-    <>
-      <ToolbarContainer>
-        <Section>
-          <FormatButton format={HeadingType.H1} />
-          <FormatButton format={HeadingType.H2} />
-        </Section>
+//   const onToggleBulletedList = useMemo(() => onToggleList(ListType.UL_LIST), [
+//     onToggleList,
+//   ])
 
-        <Section>
-          <FormatButton format={BLOCKQUOTE} />
-          <FormatButton format={CODE_BLOCK} />
-          <FormatButton format={IMAGE} onMouseDown={onInsertImage} />
-        </Section>
+//   return (
+//     <>
+//       <ToolbarContainer>
+//         <Section>
+//           <FormatButton format={HeadingType.H1} />
+//           <FormatButton format={HeadingType.H2} />
+//         </Section>
 
-        <Section>
-          <FormatButton
-            format={ListType.OL_LIST}
-            onMouseDown={onToggleNumberedList}
-          />
-          <FormatButton
-            format={ListType.UL_LIST}
-            onMouseDown={onToggleBulletedList}
-          />
-        </Section>
+//         <Section>
+//           <FormatButton format={BLOCKQUOTE} />
+//           <FormatButton format={CODE_BLOCK} />
+//           <FormatButton format={IMAGE} onMouseDown={onInsertImage} />
+//         </Section>
 
-        <Section>
-          <FormatButton
-            text="hr"
-            format={HORIZONTAL_RULE}
-            onMouseDown={onInsertHorizontalRule}
-          />
-        </Section>
+//         <Section>
+//           <FormatButton
+//             format={ListType.OL_LIST}
+//             onMouseDown={onToggleNumberedList}
+//           />
+//           <FormatButton
+//             format={ListType.UL_LIST}
+//             onMouseDown={onToggleBulletedList}
+//           />
+//         </Section>
 
-        {/* <Section>
-          <FormatButton format={ELEMENTS.EMBED} text="Embed" />
-        </Section> */}
-      </ToolbarContainer>
-    </>
-  )
-}
+//         <Section>
+//           <FormatButton
+//             text="hr"
+//             format={HORIZONTAL_RULE}
+//             onMouseDown={onInsertHorizontalRule}
+//           />
+//         </Section>
+
+//         <Section>
+//           <FormatButton format={ELEMENTS.EMBED} text="Embed" />
+//         </Section>
+//       </ToolbarContainer>
+//     </>
+//   )
+// }
