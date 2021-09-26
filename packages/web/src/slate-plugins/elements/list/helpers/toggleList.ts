35518c653c573f45ed258469ae8beebf2f3fe863
Mateusz Hadryś <hadrys.mateusz@gmail.com>
2021-09-27 01:52:14 +0200

    comment out old list helpers

--- packages/web/src/slate-plugins/elements/list/helpers/toggleList.ts
+++ packages/web/src/slate-plugins/elements/list/helpers/toggleList.ts
@@ -1,5 +1,5 @@
 import { Editor, Transforms } from "slate"
-import { isNodeTypeIn, wrapNodes, getNodesByType } from "@udecode/slate-plugins"
+// import { isNodeTypeIn, wrapNodes, getNodesByType } from "@udecode/slate-plugins"
 
 import { unwrapList } from "./unwrapList"
 
@@ -7,26 +7,20 @@ import { DEFAULT } from "../../../../slate-helpers"
 import { ListType } from "../../../../slateTypes"
 
 export const toggleList = (editor: Editor, listType: string) => {
-  if (!editor.selection) return
-
-  const isActive = isNodeTypeIn(editor, listType)
-
-  unwrapList(editor)
-
-  Transforms.setNodes(editor, {
-    type: DEFAULT,
-  })
-
-  if (!isActive) {
-    const list = { type: listType, children: [] }
-    wrapNodes(editor, list)
-
-    // get default (paragraph) nodes inside the selection that will become list items
-    const nodes = [...getNodesByType(editor, DEFAULT)]
-
-    for (const [, path] of nodes) {
-      const listItem = { type: ListType.LIST_ITEM, children: [] }
-      Transforms.wrapNodes(editor, listItem, { at: path })
-    }
-  }
+  // if (!editor.selection) return
+  // const isActive = isNodeTypeIn(editor, listType)
+  // unwrapList(editor)
+  // Transforms.setNodes(editor, {
+  //   type: DEFAULT,
+  // })
+  // if (!isActive) {
+  //   const list = { type: listType, children: [] }
+  //   wrapNodes(editor, list)
+  //   // get default (paragraph) nodes inside the selection that will become list items
+  //   const nodes = [...getNodesByType(editor, DEFAULT)]
+  //   for (const [, path] of nodes) {
+  //     const listItem = { type: ListType.LIST_ITEM, children: [] }
+  //     Transforms.wrapNodes(editor, listItem, { at: path })
+  //   }
+  // }
 }
--- packages/web/src/slate-plugins/elements/list/helpers/unwrapList.ts
+++ packages/web/src/slate-plugins/elements/list/helpers/unwrapList.ts
@@ -1,11 +1,11 @@
 import { Editor } from "slate"
-import { unwrapNodesByType } from "@udecode/slate-plugins"
+// import { unwrapNodesByType } from "@udecode/slate-plugins"
 
-import { ListType } from "../../../../slateTypes"
+// import { ListType } from "../../../../slateTypes"
 
 export const unwrapList = (editor: Editor) => {
-  unwrapNodesByType(editor, ListType.LIST_ITEM)
-  unwrapNodesByType(editor, [ListType.OL_LIST, ListType.UL_LIST], {
-    split: true,
-  })
+  // unwrapNodesByType(editor, ListType.LIST_ITEM)
+  // unwrapNodesByType(editor, [ListType.OL_LIST, ListType.UL_LIST], {
+  //   split: true,
+  // })
 }
