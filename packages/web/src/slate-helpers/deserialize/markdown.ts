01ba1f28f4e0fd405935bfa6bbf467cd29c2aeb4
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2021-11-25 22:34:42 +0100

    remove old file: slateTypes

--- packages/web/src/slate-helpers/deserialize/markdown.ts
+++ packages/web/src/slate-helpers/deserialize/markdown.ts
@@ -1,22 +1,24 @@
 import markdown from "remark-parse"
-import slate from "../remark-slate-fork"
 import unified from "unified"
+import { Descendant } from "slate"
 import {
   ELEMENT_PARAGRAPH,
   ELEMENT_BLOCKQUOTE,
   ELEMENT_LINK,
-  ELEMENT_UL_LIST,
-  ELEMENT_OL_LIST,
-  ELEMENT_LIST_ITEM,
-  ELEMENT_HEADING_3,
-  ELEMENT_HEADING_2,
-  ELEMENT_HEADING_1,
-  ELEMENT_HEADING_4,
-  ELEMENT_HEADING_5,
-  ELEMENT_HEADING_6,
-} from "../../slateTypes"
-import { Descendant } from "slate"
+  ELEMENT_UL,
+  ELEMENT_OL,
+  ELEMENT_LI,
+  ELEMENT_H1,
+  ELEMENT_H2,
+  ELEMENT_H3,
+  ELEMENT_H4,
+  ELEMENT_H5,
+  ELEMENT_H6,
+} from "@udecode/plate"
 
+import slate from "../remark-slate-fork"
+
+// TODO: this can probably be replaced by the plate markdown deserializer
 export const deserializeMarkdown = (content: string) => {
   const tree: any = unified()
     .use(markdown)
@@ -25,16 +27,16 @@ export const deserializeMarkdown = (content: string) => {
         paragraph: ELEMENT_PARAGRAPH,
         block_quote: ELEMENT_BLOCKQUOTE,
         link: ELEMENT_LINK,
-        ul_list: ELEMENT_UL_LIST,
-        ol_list: ELEMENT_OL_LIST,
-        listItem: ELEMENT_LIST_ITEM,
+        ul_list: ELEMENT_UL,
+        ol_list: ELEMENT_OL,
+        listItem: ELEMENT_LI,
         heading: {
-          1: ELEMENT_HEADING_1,
-          2: ELEMENT_HEADING_2,
-          3: ELEMENT_HEADING_3,
-          4: ELEMENT_HEADING_4,
-          5: ELEMENT_HEADING_5,
-          6: ELEMENT_HEADING_6,
+          1: ELEMENT_H1,
+          2: ELEMENT_H2,
+          3: ELEMENT_H3,
+          4: ELEMENT_H4,
+          5: ELEMENT_H5,
+          6: ELEMENT_H6,
         },
       },
     })
