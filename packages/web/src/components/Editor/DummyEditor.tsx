798176b46f1a6b3510722bd63cccead925aa684b
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2021-11-29 23:06:02 +0100

    refactor old DummyEditor to use new generalized editor component

--- packages/web/src/components/Editor/DummyEditor.tsx
+++ packages/web/src/components/Editor/DummyEditor.tsx
@@ -1,103 +1,55 @@
-import React, { useRef, useState } from "react"
-import isHotkey from "is-hotkey"
-import { Plate, usePlateEditorRef, usePlateEventId } from "@udecode/plate"
+import { useCallback } from "react"
+import { usePlateEditorRef, usePlateEventId } from "@udecode/plate"
 import { Descendant } from "slate"
 
 import {
   CreateDocumentFn,
+  DEFAULT_EDITOR_VALUE,
   useDocumentsAPI,
   useTabsState,
 } from "../MainProvider"
-import { Toolbar } from "../Toolbar"
 import { serialize } from "../Editor/serialization"
 import { useTabsDispatch } from "../MainProvider"
 
-import { StyledTitleNamingInput } from "./TitleInput"
-import pluginsList from "./pluginsList"
-import {
-  EditableContainer,
-  OuterContainer,
-  InnerContainer,
-} from "./styledComponents"
-
-// TODO: remove remaining duplication with Editor
-
-const DummyTitleInput: React.FC<{
-  createDocumentAndReplaceTab: (
-    values: Parameters<CreateDocumentFn>[0]
-  ) => Promise<void>
-}> = ({ createDocumentAndReplaceTab }) => {
-  const [value, setValue] = useState<string>("")
-  const titleRef = useRef<HTMLTextAreaElement | null>(null)
-
-  const onKeyDown = (event: React.KeyboardEvent) => {
-    // Use the saving keyboard shortcut to apply title change
-    if (isHotkey("mod+s", event)) {
-      console.log("saving")
-      event.preventDefault()
-      onRename(value)
-    }
-
-    // if (isHotkey(["Enter", "Esc"], event)) {
-    //   // prevent the line break from being inserted into the title (TODO: some version of this behavior might be desirable)
-    //   event.preventDefault()
-    //   // TODO: insert an empty block at the start of the editor
-    //   Transforms.insertNodes(editor, createEmptyNode(), {
-    //     at: [0],
-    //     select: true,
-    //   })
-    //   // move focus to the editor (as if the title was a part of the editable area) - this will automatically trigger a rename
-    //   ReactEditor.focus(editor)
-    //   return false
-    // }
-    return true
-  }
-
-  const onChange = (newValue: string) => {
-    setValue(newValue)
-  }
-
-  const onRename = async (newValue: string) => {
-    if (newValue.trim() !== "") {
-      createDocumentAndReplaceTab({ parentGroup: null, title: newValue })
-    }
-  }
-
-  const getTitleInputProps = () => ({ onKeyDown, onChange, onRename, value })
-
-  return <StyledTitleNamingInput ref={titleRef} {...getTitleInputProps()} />
-}
+import { EditorComponent } from "."
 
 export const DummyEditor = () => {
   const editor = usePlateEditorRef(usePlateEventId("focus"))
-
   const { createDocument } = useDocumentsAPI()
   const { currentTab } = useTabsState()
   const tabsDispatch = useTabsDispatch()
 
-  const createDocumentAndReplaceTab = async (
-    values: Parameters<CreateDocumentFn>[0]
-  ) => {
-    // create document and use current content (switch to it in a new tab as well)
-    const newDocument = await createDocument(values, {
-      switchToDocument: false,
-      switchToGroup: true,
-    })
-    // replace old placeholder tab
-    tabsDispatch({
-      type: "replace-tab",
-      tab: {
-        tabId: currentTab,
-        tabType: "cloudDocument",
-        documentId: newDocument.id,
-        keep: true,
-      },
-      switch: true,
-    })
-  }
+  const createDocumentAndReplaceTab = useCallback(
+    async (values: Parameters<CreateDocumentFn>[0]) => {
+      // create document and use current content (switch to it in a new tab as well)
+      const newDocument = await createDocument(values, {
+        switchToDocument: false,
+        switchToGroup: true,
+      })
+      // replace old placeholder tab
+      tabsDispatch({
+        type: "replace-tab",
+        tab: {
+          tabId: currentTab,
+          tabType: "cloudDocument",
+          documentId: newDocument.id,
+          keep: true,
+        },
+        switch: true,
+      })
+    },
+    [createDocument, currentTab, tabsDispatch]
+  )
+
+  const onRename = useCallback(
+    (value: string) => {
+      createDocumentAndReplaceTab({ parentGroup: null, title: value })
+    },
+    [createDocumentAndReplaceTab]
+  )
 
   // TODO: remove duplication with onRename in DummyTitleInput
-  const onSave = async () => {
+  const onSave = useCallback(async () => {
     // TODO: remove duplication with saveDocument function logic
     if (editor === undefined) {
       console.error("Can't save, the editor is undefined")
@@ -113,41 +65,15 @@ export const DummyEditor = () => {
         content: serializedContent,
       })
     }
-  }
-
-  const editableProps = {
-    placeholder: "Start writing…",
-    spellCheck: /* isSpellCheckEnabled */ false, // TODO: change this back when I restore userdata
-    onBlur: (e) => {
-      // TODO: if you close the window or reload without clicking on something else the blur doesn't trigger and the content doesn't get saved
-      onSave()
-    },
-  }
+  }, [createDocumentAndReplaceTab, editor])
 
   return (
-    <OuterContainer
-      onKeyDown={(e) => {
-        if (isHotkey("mod+s", e)) {
-          e.preventDefault()
-          onSave()
-        }
-      }}
-    >
-      <InnerContainer>
-        <EditableContainer>
-          <Plate
-            id="main"
-            plugins={pluginsList}
-            editableProps={editableProps}
-            // onChange={onChange} TODO: maybe use this to trigger document creation (or any click inside the editor)
-          >
-            <DummyTitleInput
-              createDocumentAndReplaceTab={createDocumentAndReplaceTab}
-            />
-            <Toolbar />
-          </Plate>
-        </EditableContainer>
-      </InnerContainer>
-    </OuterContainer>
+    <EditorComponent
+      key={currentTab} // Necessary to reload the component on id change
+      saveDocument={onSave}
+      renameDocument={onRename}
+      title={""}
+      content={DEFAULT_EDITOR_VALUE}
+    />
   )
 }
