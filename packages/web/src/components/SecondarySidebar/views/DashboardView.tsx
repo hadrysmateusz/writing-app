5d5cc9e3d2d5421e4037cc31213f0af8b2c7fcfb
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2021-11-29 23:05:40 +0100

    don't render keywords section for tab tabs other than cloudDocument

--- packages/web/src/components/SecondarySidebar/views/DashboardView.tsx
+++ packages/web/src/components/SecondarySidebar/views/DashboardView.tsx
@@ -5,8 +5,15 @@ import { Section, SecondarySidebarViewContainer } from "../../SidebarCommon"
 import TextStats from "../TextStats"
 import { Outline } from "../Outline"
 import Keywords from "../Keywords"
+import { useTabsState } from "../../MainProvider"
 
 export const DashboardView: React.FC = () => {
+  const { tabs, currentTab } = useTabsState()
+
+  let currentTabObj = tabs[currentTab]
+  // TODO: probably precompute this and expose in useTabsState hook
+  const currentTabType = currentTabObj.tabType
+
   return (
     <SecondarySidebarViewContainer>
       <Section title="Progress">
@@ -15,11 +22,14 @@ export const DashboardView: React.FC = () => {
         </div>
       </Section>
 
-      <Section title="Keywords">
-        <div>
-          <Keywords />
-        </div>
-      </Section>
+      {/* Only render keywords section for cloud documents as the system doesn't support other types  */}
+      {currentTabType === "cloudDocument" ? (
+        <Section title="Keywords">
+          <div>
+            <Keywords />
+          </div>
+        </Section>
+      ) : null}
 
       <Section title="Outline">
         <div>
