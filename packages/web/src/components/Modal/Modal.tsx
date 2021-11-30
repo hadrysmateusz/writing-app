e798ad2defe2948decb615bcbb6366ffcad3fe54
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2021-11-30 01:02:59 +0100

    chore: add comment

--- packages/web/src/components/Modal/Modal.tsx
+++ packages/web/src/components/Modal/Modal.tsx
@@ -42,6 +42,8 @@ const ModalBox = styled.div`
   position: relative;
 `
 
+// TODO: move to a global provider based approach where there is one modal provider handling the modal's state and the consumers use a hook that registers a particular modal with an id in useEffect (this way we can check and prevent duplicate ids) this would eliminate the need for using a Modal component passed from a hook and instead only use a single hook that also exposes a getModalProps() function for the modal content component (maybe a single, globally exposed modal component could be used which would handle some of the interaction with the provider and the portal component)
+
 // TODO: the modal almost definitely needs work: accessibility, focus trapping, options, different ways of closing it, etc.
 export const Modal: React.FC<{ onRequestClose: (...args: any) => any }> = ({
   children,
