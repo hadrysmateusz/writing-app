d14fefe1970e7f9ce1511b81a258e0c30c2aa0fe
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2021-12-02 03:28:47 +0100

    add basic create file handler in main process

--- packages/desktop/src/main.ts
+++ packages/desktop/src/main.ts
@@ -283,6 +283,61 @@ ipcMain.handle("WRITE_FILE", async (_event, payload) => {
   }
 })
 
+ipcMain.handle(
+  "CREATE_FILE",
+  async (
+    _event,
+    payload: {
+      name?: string
+    } = {}
+  ) => {
+    const { name = undefined } = payload
+
+    let defaultPath = path.join(os.homedir(), name ?? "")
+
+    const filter = filters["md"]
+
+    const dialogRes = await dialog.showSaveDialog({
+      title: "Create Document",
+      defaultPath,
+      buttonLabel: "Create",
+      filters: [filter],
+    })
+
+    const filePath = dialogRes.filePath
+
+    if (!filePath || dialogRes.canceled === true) {
+      return {
+        status: DialogStatus.CANCELED,
+        error: null,
+        data: null,
+      }
+    }
+
+    const fileExists =
+      fs.pathExistsSync(filePath) && fs.lstatSync(filePath).isFile()
+
+    try {
+      // TODO: investigate different encodings and flags - do I need to do more to make this work with all files
+      await fs.writeFile(filePath, "")
+      return {
+        status: DialogStatus.SUCCESS,
+        error: null,
+        data: {
+          filePath,
+          overwritten: fileExists,
+        },
+      }
+    } catch (error) {
+      return {
+        status: DialogStatus.ERROR,
+        error: "An error ocurred writing to file :" + error.message,
+        data: null,
+      }
+    }
+  }
+)
+
 ipcMain.handle("ADD_PATH", async (_event, _payload) => {
   // TODO: better default path
   // TODO: save the last used path for later
