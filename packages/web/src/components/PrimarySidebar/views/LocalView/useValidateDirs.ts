1ca4b25ccd09b3f775fbe5a644f64073ac228d39
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2021-12-08 15:55:05 +0100

    extract dir validation logic to useValidateDirs

---
+++ packages/web/src/components/PrimarySidebar/views/LocalView/useValidateDirs.ts
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react"
+
+import { useLocalSettings } from "../../../LocalSettings"
+import { useDatabase } from "../../../Database"
+import { useCurrentUser } from "../../../Auth"
+
+import { ValidatePathsObj } from "./types"
+
+// TODO: figure out a name for this step
+export const useValidateDirs = () => {
+  const db = useDatabase()
+  const currentUser = useCurrentUser()
+
+  const { updateLocalSetting } = useLocalSettings()
+
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  // Don't use __setDirs directly after initial load, use updateDirs instead
+  const [dirs, __setDirs] = useState<ValidatePathsObj[]>()
+
+  useEffect(() => {
+    ;(async () => {
+      setIsLoading(true)
+
+      const localSettingsDoc = await db.local_settings
+        .findOne(currentUser.username)
+        .exec()
+
+      if (localSettingsDoc === null) {
+        throw new Error(
+          `Couldn't find a local settings doc for user: ${currentUser.username}`
+        )
+      }
+
+      const { localDocPaths } = localSettingsDoc
+
+      const ipcResponse = await window.electron.invoke("VALIDATE_PATHS", {
+        paths: localDocPaths,
+      })
+
+      console.log(ipcResponse)
+
+      if (ipcResponse.status === "success") {
+        const { dirs } = ipcResponse.data
+        __setDirs(dirs)
+      } else {
+        // TODO: handle this
+        console.warn("something went wrong")
+      }
+
+      setIsLoading(false)
+    })()
+  }, [currentUser.username, db.local_settings])
+
+  const updateDirs = async (newDirs: ValidatePathsObj[]) => {
+    __setDirs(newDirs)
+    // update the persisted local doc paths
+    const newLocalDocPaths = newDirs.map((dir) => dir.path)
+    await updateLocalSetting("localDocPaths", newLocalDocPaths)
+  }
+
+  return { isLoading, dirs, updateDirs }
+}
