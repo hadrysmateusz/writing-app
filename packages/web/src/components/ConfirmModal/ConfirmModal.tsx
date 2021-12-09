b45bfee1e3c55ad1096fa9f9743b6448c99caeea
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2021-12-09 15:51:03 +0100

    create ConfirmModal

---
+++ packages/web/src/components/ConfirmModal/ConfirmModal.tsx
@@ -0,0 +1,91 @@
+import React, { useRef } from "react"
+import { ToggleableHooks } from "../../hooks"
+import { Button, ButtonVariants } from "../Button"
+import { useModal, ModalContainer, ModalButtonsContainer } from "../Modal"
+import { ModalContentProps } from "../Modal/types"
+
+export type ConfirmModalOpenReturnValue = boolean
+export type ConfirmModalProps = {}
+export type ConfirmModalContentProps = ModalContentProps<
+  ConfirmModalOpenReturnValue,
+  ConfirmModalProps
+>
+export type ConfirmModalOptions =
+  ToggleableHooks<ConfirmModalOpenReturnValue> & {
+    onConfirm?: () => void
+    onCancel?: () => void
+  }
+
+export function useConfirmModal(options: ConfirmModalOptions = {}) {
+  const { onConfirm, onCancel, ...modalOptions } = options
+  return useModal<ConfirmModalOpenReturnValue, ConfirmModalProps>(
+    false,
+    {},
+    {
+      ...modalOptions,
+      onAfterClose: (resolveValue) => {
+        // call onConfirm or onCancel based on resolveValue if these hooks are provided
+        if (resolveValue === undefined || resolveValue === false) {
+          onCancel && onCancel()
+        } else {
+          onConfirm && onConfirm()
+        }
+        // call the onAfterClose hook from options if it's provided
+        modalOptions.onAfterClose && modalOptions.onAfterClose(resolveValue)
+      },
+    }
+  )
+}
+
+export function getConfirmModalContent({
+  promptMessage = "Are you sure?",
+  confirmMessage = "OK",
+  cancelMessage = "Cancel",
+  confirmButtonVariant = "danger", // TODO: replace with primary when it's fixed
+}: {
+  promptMessage?: string
+  confirmMessage?: string
+  cancelMessage?: string
+  confirmButtonVariant?: ButtonVariants
+} = {}) {
+  const ConfirmModalContent: React.FC<ConfirmModalContentProps> = ({
+    close,
+  }) => {
+    const confirmBtnRef = useRef<HTMLButtonElement | null>(null)
+    const cancelBtnRef = useRef<HTMLButtonElement | null>(null)
+
+    const handleConfirm = (event: React.FormEvent) => {
+      event.preventDefault()
+      close(true)
+    }
+
+    const handleCancel = (event: React.FormEvent) => {
+      event.preventDefault()
+      close(false)
+    }
+
+    // useEffect(() => {
+    //   confirmBtnRef?.current?.focus()
+    // }, [])
+
+    return (
+      <ModalContainer>
+        <p style={{ marginTop: "4px" }}>{promptMessage}</p>
+        <ModalButtonsContainer>
+          <Button
+            onClick={handleConfirm}
+            variant={confirmButtonVariant}
+            ref={confirmBtnRef}
+          >
+            {confirmMessage}
+          </Button>
+          <Button onClick={handleCancel} ref={cancelBtnRef}>
+            {cancelMessage}
+          </Button>
+        </ModalButtonsContainer>
+      </ModalContainer>
+    )
+  }
+
+  return ConfirmModalContent
+}
