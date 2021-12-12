15b3cd6d7aadd1a61eb7a0c4bb1022824b79738b
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2021-12-12 04:35:42 +0100

    create universal TextInput component

---
+++ packages/web/src/components/TextInput/TextInput.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components/macro"
+
+// TODO: refine these styles
+export const TextInput = styled.input`
+  background: var(--dark-200);
+  color: var(--light-300);
+
+  padding: 6px 10px;
+
+  min-width: 0;
+
+  width: 100%;
+
+  font-family: inherit;
+  font-size: inherit;
+
+  border: 1px solid var(--dark-500);
+  border-radius: 3px;
+
+  ::selection {
+    background: var(--selection-color);
+  }
+
+  :focus,
+  :focus-within {
+    border-color: var(--dark-600);
+    color: var(--light-400);
+  }
+`
