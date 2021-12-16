41db5144d8f86aab1c07c0fb77707086a53b2f0e
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2021-12-16 04:19:09 +0100

    overhaul Autocomplete component

--- packages/web/src/components/Autocomplete/Autocomplete.styles.ts
+++ packages/web/src/components/Autocomplete/Autocomplete.styles.ts
@@ -5,36 +5,70 @@ import { customScrollbar } from "../../style-utils"
 export const AutocompleteContainer = styled.div`
   .Autocomplete_EmptyState {
     color: var(--light-100);
-    padding-top: 10px;
+    padding: 4px 10px 4px;
   }
   .Autocomplete_SuggestionsContainer {
     color: var(--light-500);
 
-    padding-top: 6px;
+    /* padding-top: 6px; */
     border: none;
     list-style: none;
     margin: 0;
-    max-height: 139px;
+    max-height: 165px;
     overflow-y: auto;
     padding-left: 0;
     width: 100%;
+    display: block;
 
     ${customScrollbar}
   }
+  .Autocomplete_ConfirmPrompt {
+    border-top: 1px solid var(--dark-500);
+    color: var(--light-100);
+    padding: 10px 6px 4px 16px;
+    margin: 6px -6px 0;
+  }
 `
 
-const emphasizedSuggestionItem = css`
-  color: var(--light-600);
-  background-color: var(--dark-500);
-  cursor: pointer;
-`
+const emphasizedSuggestionItem = (isDisabled: boolean) =>
+  !isDisabled
+    ? css`
+        color: var(--light-600);
+        background-color: var(--dark-500);
+      `
+    : css`
+        background-color: #2c2c2c; // TODO: create more/better colors
+      `
 
-export const SuggestionItem = styled.li<{ isActive: boolean }>`
+export const SuggestionItem = styled.li<{
+  isActive: boolean
+  isDisabled: boolean
+}>`
   border-radius: 3px;
   padding: 8px 10px;
+  user-select: none;
+  cursor: pointer;
 
-  ${(p) => p.isActive && emphasizedSuggestionItem}
-  :hover {
-    ${emphasizedSuggestionItem}
-  }
+  scroll-padding-top: 20px;
+
+  ${(p) =>
+    p.isDisabled &&
+    css`
+      color: var(--light-300);
+      cursor: default;
+    `}
+
+  ${(p) => p.isActive && emphasizedSuggestionItem(p.isDisabled)}
+
+  ${(p) =>
+    css`
+      :hover {
+        ${emphasizedSuggestionItem(p.isDisabled)};
+        ${p.isDisabled &&
+        !p.isActive &&
+        css`
+          background-color: transparent;
+        `}
+      }
+    `}
 `
--- packages/web/src/components/Autocomplete/Autocomplete.tsx
+++ packages/web/src/components/Autocomplete/Autocomplete.tsx
@@ -1,24 +1,44 @@
-import React, { forwardRef, useEffect, useState } from "react"
+import React, { useCallback, useEffect, useRef, useState } from "react"
+import { useIsHovered } from "../../hooks"
+import { scrollToShow } from "../../utils"
 import { TextInput } from "../TextInput"
 
 import { AutocompleteContainer, SuggestionItem } from "./Autocomplete.styles"
 
-export type Option = { value: string | null; label: string }
+// TODO: maybe make this a generic to support any value types
+// TODO: maybe add additional optional fields to control how the option should be displayed
+export type Option = { value: string | null; label: string; disabled?: boolean }
 
-export const Autocomplete = forwardRef<
-  HTMLInputElement,
-  {
-    suggestions: Option[]
-    submit: (value: Option) => void
-    /**
-     * Force the placeholder to be a custom string instead of currently selected option
-     */
-    placeholder?: string
-  }
->(({ suggestions, submit, placeholder }, inputRef) => {
+// TODO: improve the disabled items system (perhaps by skipping over disabled items when using arrow keys (this will require handling many edge cases and probably a recursive/looping function to handle multiple disabled options in a row))
+// TODO: support autocompletes without option to create (hide create prompt, make create function optional etc.)
+
+export const Autocomplete: React.FC<{
+  suggestions: Option[]
+  submit: (option: Option) => void
+  create: (value: string) => void
+  /**
+   * Force the placeholder to be a custom string instead of currently selected option
+   */
+  placeholder?: string
+  /**
+   * Prompt to display when there are no matching options for the provided query
+   */
+  createNewPrompt?: string
+  inputRef: React.MutableRefObject<HTMLInputElement | null>
+}> = ({
+  suggestions,
+  submit,
+  create,
+  placeholder,
+  createNewPrompt = "Press 'Enter' to create new",
+  inputRef,
+}) => {
   const [filteredSuggestions, setFilteredSuggestions] = useState(suggestions)
   const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(0)
   const [inputValue, setInputValue] = useState("")
+
+  const suggestionsContainerRef = useRef<HTMLUListElement | null>(null)
+  const currentlyActiveElementRef = useRef<HTMLLIElement | null>(null)
 
   useEffect(() => {
     // Update filteredSuggestions to match new suggestions from parent
@@ -37,11 +57,11 @@ export const Autocomplete = forwardRef<
     })
   }, [filteredSuggestions])
 
-  const reset = () => {
+  const reset = useCallback(() => {
     setInputValue("")
     setFilteredSuggestions(suggestions)
     setActiveSuggestionIndex(0)
-  }
+  }, [suggestions])
 
   const onChange = (e) => {
     // Filter out suggestions that don't contain the user's input
@@ -56,66 +76,137 @@ export const Autocomplete = forwardRef<
     setActiveSuggestionIndex(0)
   }
 
+  const wrappedSubmit = useCallback(
+    (option: Option) => {
+      if (option.disabled === true) {
+        return
+      } else {
+        reset()
+        submit(option)
+      }
+    },
+    [reset, submit]
+  )
+
   const onSuggestionClick = (option: Option) => {
-    reset()
-    submit(option)
+    wrappedSubmit(option)
   }
 
-  const handleKeyDown = (event: React.KeyboardEvent) => {
-    // TODO: scroll when the selected item gets too low or too high
-    switch (event.code) {
-      case "ArrowUp": {
-        event.preventDefault()
-        console.log("ArrowUp")
-        setActiveSuggestionIndex((prev) => Math.max(0, (prev -= 1)))
-        break
-      }
-      case "ArrowDown": {
-        event.preventDefault()
-        console.log("ArrowDown")
-        setActiveSuggestionIndex((prev) =>
-          Math.min(filteredSuggestions.length - 1, (prev += 1))
-        )
-        break
-      }
-      case "Enter": {
-        event.preventDefault()
-
-        reset()
-
-        if (filteredSuggestions[activeSuggestionIndex]) {
-          // submit selected option
-          submit(filteredSuggestions[activeSuggestionIndex])
-        } else {
-          // submit new option representing a tag to be created
-          submit({ value: null, label: inputValue })
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      switch (event.code) {
+        case "ArrowUp": {
+          event.stopPropagation()
+          event.preventDefault()
+          setActiveSuggestionIndex((prev) => Math.max(0, (prev -= 1)))
+          break
         }
-        break
+        case "ArrowDown": {
+          event.stopPropagation()
+          event.preventDefault()
+          setActiveSuggestionIndex((prev) =>
+            Math.min(filteredSuggestions.length - 1, (prev += 1))
+          )
+          break
+        }
+        case "Enter": {
+          event.stopPropagation()
+          event.preventDefault()
+
+          if (filteredSuggestions[activeSuggestionIndex]) {
+            // submit selected option
+            wrappedSubmit(filteredSuggestions[activeSuggestionIndex])
+          } else {
+            // create and submit the new option (all handled by 'create')
+            const __inputValue = inputValue
+            reset()
+            create(__inputValue)
+          }
+          break
+        }
+        // TODO: use esc to close
+      }
+    },
+    [
+      activeSuggestionIndex,
+      create,
+      filteredSuggestions,
+      inputValue,
+      reset,
+      wrappedSubmit,
+    ]
+  )
+
+  const { getHoverContainerProps, isHovered } = useIsHovered()
+
+  useEffect(() => {
+    if (isHovered) {
+      const inputRefCurrent = inputRef?.current
+      inputRefCurrent?.focus()
+      window.addEventListener("keydown", handleKeyDown)
+      return () => {
+        inputRefCurrent?.blur()
+        window.removeEventListener("keydown", handleKeyDown)
       }
     }
-  }
+    return undefined
+  }, [handleKeyDown, inputRef, isHovered])
 
+  useEffect(() => {
+    const suggestionsContainer = suggestionsContainerRef.current
+    const currentlyActiveItem = currentlyActiveElementRef.current
+
+    if (!currentlyActiveItem || !suggestionsContainer) {
+      return
+    }
+
+    setTimeout(() => {
+      scrollToShow(currentlyActiveItem, suggestionsContainer)
+    }, 0)
+  }, [activeSuggestionIndex])
+
+  const isEmpty = !(filteredSuggestions.length > 0)
+  const isDisabledItemActive =
+    filteredSuggestions[activeSuggestionIndex]?.disabled === true
+
+  // TODO: add a way to create new by clicking
   return (
-    <AutocompleteContainer>
+    <AutocompleteContainer
+      onClick={(e) => e.stopPropagation()}
+      {...getHoverContainerProps()}
+    >
       <TextInput
         ref={inputRef}
         type="text"
         onChange={onChange}
-        onKeyDown={handleKeyDown} // TODO: use arrow keys to select options and enter/esc to select/close
         value={inputValue}
         placeholder={
           placeholder || filteredSuggestions[activeSuggestionIndex]?.label
         }
       />
 
-      {filteredSuggestions.length > 0 ? (
-        <ul className="Autocomplete_SuggestionsContainer">
+      <div style={{ height: "6px" }} />
+
+      {!isEmpty ? (
+        <ul
+          className="Autocomplete_SuggestionsContainer"
+          ref={suggestionsContainerRef}
+        >
           {filteredSuggestions.map((suggestion, index) => {
             return (
               <SuggestionItem
                 key={suggestion.value}
-                onClick={() => onSuggestionClick(suggestion)}
+                onClick={(e) => {
+                  e.stopPropagation()
+                  onSuggestionClick(suggestion)
+                }}
                 isActive={index === activeSuggestionIndex}
+                isDisabled={suggestion.disabled === true}
+                ref={
+                  index === activeSuggestionIndex
+                    ? currentlyActiveElementRef
+                    : undefined
+                }
               >
                 {suggestion.label}
               </SuggestionItem>
@@ -124,11 +215,16 @@ export const Autocomplete = forwardRef<
         </ul>
       ) : (
         <div className="Autocomplete_EmptyState">
-          {inputValue.trim() === ""
-            ? "Start typing"
-            : "Press 'Enter' to create new tag"}
+          {inputValue.trim() === "" ? "Start typing" : createNewPrompt}
         </div>
       )}
+      {!isEmpty ? (
+        <div className="Autocomplete_ConfirmPrompt">
+          {isDisabledItemActive
+            ? "Can't select. This option is disabled"
+            : "Press 'Enter' to confirm selection"}
+        </div>
+      ) : null}
     </AutocompleteContainer>
   )
-})
+}
