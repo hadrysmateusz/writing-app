fa720a45c7e00ace33b445a7a70098d0e23f80a8
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2021-12-27 03:03:37 +0100

    prettify withDelayRender

--- packages/web/src/withDelayRender.tsx
+++ packages/web/src/withDelayRender.tsx
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from "react"
 
 // TODO: probably needs many improvements and optimizations (might be able to use Suspense)
-export const withDelayRender = (
-  delay: number,
-  CAlt: React.FC | null = null
-) => (C: React.FC): React.FC => (props) => {
-  const [isDone, setIsDone] = useState(delay === 0)
+export const withDelayRender =
+  (delay: number, CAlt: React.FC | null = null) =>
+  (C: React.FC): React.FC =>
+  (props) => {
+    const [isDone, setIsDone] = useState(delay === 0)
 
-  useEffect(() => {
-    // render immediately if delay is set to 0
-    if (delay === 0) return
+    useEffect(() => {
+      // render immediately if delay is set to 0
+      if (delay === 0) return
 
-    const timeoutId = setTimeout(() => setIsDone(true), delay)
-    return () => clearTimeout(timeoutId)
-  }, [])
+      const timeoutId = setTimeout(() => setIsDone(true), delay)
+      return () => clearTimeout(timeoutId)
+    }, [])
 
-  return isDone ? <C {...props} /> : CAlt === null ? null : <CAlt {...props} />
-}
+    return isDone ? (
+      <C {...props} />
+    ) : CAlt === null ? null : (
+      <CAlt {...props} />
+    )
+  }
