f66e99d43b7a740c03bba12b8b9076900549f5a1
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2022-01-05 22:44:40 +0100

    create useQuery hook

--- packages/web/src/hooks/useQuery.ts
+++ packages/web/src/hooks/useQuery.ts
@@ -6,13 +6,13 @@ import { Sorting, useSorting } from "../components/SortingProvider"
 
 // TODO: create a sorting-less version
 
-export type QueryCreator<RxDocumentType, RxQueryResult> = (
+export type QueryCreatorWithSorting<RxDocumentType, RxQueryResult> = (
   db: MyDatabase,
   sorting: Sorting
 ) => RxQuery<RxDocumentType, RxQueryResult>
 
 export const useQueryWithSorting = <RxDocumentType, RxQueryResult>(
-  queryCreator: QueryCreator<RxDocumentType, RxQueryResult>,
+  queryCreator: QueryCreatorWithSorting<RxDocumentType, RxQueryResult>,
   dependencies: any[]
 ) => {
   const db = useDatabase()
@@ -22,6 +22,25 @@ export const useQueryWithSorting = <RxDocumentType, RxQueryResult>(
     () => queryCreator(db, sorting),
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [db, queryCreator, sorting, ...dependencies]
+  )
+
+  return query
+}
+
+export type QueryCreator<RxDocumentType, RxQueryResult> = (
+  db: MyDatabase
+) => RxQuery<RxDocumentType, RxQueryResult>
+
+export const useQuery = <RxDocumentType, RxQueryResult>(
+  queryCreator: QueryCreator<RxDocumentType, RxQueryResult>,
+  dependencies: any[]
+) => {
+  const db = useDatabase()
+
+  const query = useMemo(
+    () => queryCreator(db),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [db, queryCreator, ...dependencies]
   )
 
   return query
